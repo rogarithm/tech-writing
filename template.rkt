513246014a044c150ad464b484ea5fdfd4d9767f
rogarithm <rogarithm@gmail.com>
2020-07-21 18:03:56 +0900

    answer, first attempt

--- template.rkt
+++ template.rkt
@@ -12,45 +12,65 @@ wish-lists
 
 ◊body{
 They utilized his findings.
-◊{They used his findings.}
+They used his findings.
+
 You have to accomplish the test next month.
-◊ 
+You have to finish the test next month.
+
 You can also employ separate gating circuits.
-◊ 
+You can also use separate gating circuits.
+
 Ascertain that they have finished it.
-◊ 
+Make sure that they have finished it.
+
 The heat accomplishes the breakup.
-◊ 
+The heat results the breakup.
+
 Each room will be equipped with two humidifiers.
-◊ 
+Each room will have two humidifiers.
+
 Volume 3 presents the following information.
-◊ 
+Volume 3 shows the following information.
+
 This chart illustrates where the problem is.
-◊ 
+This chart shows where the problem is.
+
 One-bit logic requires less power.
-◊ 
+One-bit logic takes less power.
+
 This will terminate the tests.
-◊ 
+This will end the tests.
+
 The cost may prove prohibitive.
-◊ 
+The cost may be too expensive.
+
 The points on the graph represent the new data.
-◊ 
+The points on the graph show the new data.
+
 During evaporation, the composition of the vapor varies continually.
-◊ 
+During evaporation, the composition of the vapor differs continually.
+
 The trucks have been purchased.
-◊ 
+The trucks have been bought.
+
 We have established three indoctrination centers.
-◊ 
+We have built three indoctrination centers.
+
 The trailers greatly enhance mobility.
-◊ 
+The trailers greatly improve mobility.
+
 Rotate the handcrank fully clockwise.
-◊ 
+Rotate the handcrank fully clockwise.
+
 Perform this test first.
-◊ 
+Do this test first.
+
 Everyone is aware of this problem.
-◊ 
+Everyone knows this problem.
+
 The air conditioners will maintain the temperature.
-◊ 
+The air conditioners will remain the temperature.
+
 A simple design also minimizes costs.
-◊ 
+A simple design also minimizes costs.
 }
