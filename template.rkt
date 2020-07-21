0820c57cd4ada1f024225d9819a595efd0dd2030
Sehun Kim <sehun@sh.local>
2020-07-21 15:40:41 +0900

    initialize repository

---
+++ template.rkt
@@ -0,0 +1,56 @@
+#lang pollen
+◊(require txexpr)
+◊(define ( . elements)
+(txexpr 'Ans empty elements))
+goal: automate repeatidely doing things
+wish-lists
+- recursively replace strings
+
+
+◊head{Lean verbs}
+◊sub-head{Correct the following sentences.}
+
+◊body{
+They utilized his findings.
+◊{They used his findings.}
+You have to accomplish the test next month.
+◊ 
+You can also employ separate gating circuits.
+◊ 
+Ascertain that they have finished it.
+◊ 
+The heat accomplishes the breakup.
+◊ 
+Each room will be equipped with two humidifiers.
+◊ 
+Volume 3 presents the following information.
+◊ 
+This chart illustrates where the problem is.
+◊ 
+One-bit logic requires less power.
+◊ 
+This will terminate the tests.
+◊ 
+The cost may prove prohibitive.
+◊ 
+The points on the graph represent the new data.
+◊ 
+During evaporation, the composition of the vapor varies continually.
+◊ 
+The trucks have been purchased.
+◊ 
+We have established three indoctrination centers.
+◊ 
+The trailers greatly enhance mobility.
+◊ 
+Rotate the handcrank fully clockwise.
+◊ 
+Perform this test first.
+◊ 
+Everyone is aware of this problem.
+◊ 
+The air conditioners will maintain the temperature.
+◊ 
+A simple design also minimizes costs.
+◊ 
+}
