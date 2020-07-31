a8b93f4fb76207773a44f1c6b83bda88457426b1
rogarithm <rogarithm@gmail.com>
2020-08-01 01:36:16 +0900

    completed exercise for week 6

--- writing-exercise/ex-6.rkt
+++ writing-exercise/ex-6.rkt
@@ -3,43 +3,43 @@
 Auxiliary verbs
 Correct the following sentences.
 
-The device is repairable.
+1. The device is repairable.
  The device can be repaired.
-It is possible to feed either two or three branches.
- 
-He was instrumental in creating the document.
- 
-It is necessary to find the correct ratio.
+2. It is possible to feed either two or three branches.
+ You can feed either two or three branchs.
+3. He was instrumental in creating the document.
+ He creates the document instrumentally.
+4. It is necessary to find the correct ratio.
  We need to find the correct ratio.
-It is essential that the data be within the range.
- 
-This synthesizer can generate words.
+5. It is essential that the data be within the range.
+ The data should be within the range.
+6. This synthesizer can generate words.
  This synthesizer can make words.
-The section contains information on adjusting the assembly.
- 
-An attempt will be made to reduce costs.
- We will attempt to reduce costs.
+7. The section contains information on adjusting the assembly.
+ The section informs how to adjust the assembly.
+8. An attempt will be made to reduce costs.
+ We will try to reduce costs.
 
 Ponderous writing
 Correct the following sentences.
 
-We will supply three categories of reports.
+1. We will supply three categories of reports.
  We will supply three types of reports.
-Further examination brought out the cause.
+2. Further examination brought out the cause.
  Further research brought out the cause.
-Two factors cause the deviation in resistance.
-
-The information is within Volume 3.
+3. Two factors cause the deviation in resistance.
+The deviation in resistance is caused by two factors.
+4. The information is within Volume 3.
  The information is in Volume 3. 
-Notify the contractor when you ship the items listed herein.
- 
-The electron might still fail to be collected.
- 
-It waterproofs the material so that leaks do not occur.
+5. Notify the contractor when you ship the items listed herein.
+ Inform the contractor when you ship the items listed herein.
+6. The electron might still fail to be collected.
+ The electron might still not be collected.
+7. It waterproofs the material so that leaks do not occur.
  It waterproofs the material so that it doesn't leak.
-These vehicles are being prepared for shipment.
- 
-The concentrator indicates at overflow capacity.
+8. These vehicles are being prepared for shipment.
+ These vehicles are ready for shipment.
+9. The concentrator indicates at overflow capacity.
  The concentrator points at overflow capacity.
-Some cost thousands of dollars, while others are priced in the range of cost millions.
- Some cost thousands of dollars, while others are cost millions.
+10. Some cost thousands of dollars, while others are priced in the range of cost millions.
+ Some cost thousands of dollars, while others cost millions.
--- writing-exercise/fb-5.rkt
+++ writing-exercise/fb-5.rkt
@@ -5,43 +5,63 @@ Correct the following sentences.
 
 They utilized his findings.
  They used his findings.
+
 You have to accomplish the test next month.
  You have to make the test next month.
+
 You can also employ separate gating circuits.
  You can also use separate gating circuits. 
+
 Ascertain that they have finished it.
  Check that they have finished it.
+
 The heat accomplishes the breakup.
  The heat causes the breakup.
+
 Each room will be equipped with two humidifiers.
  Each room has two humidifiers.
+
 Volume 3 presents the following information.
  Volume 3 gives the following information. 
+
 This chart illustrates where the problem is.
  This chart shows where the problem is.
+
 One-bit logic requires less power.
  One-bit logic needs less power. 
+
 This will terminate the tests.
  This will end the tests. 
+
 The cost may prove prohibitive.
  The cost may be prohibitive.
+
 The points on the graph represent the new data.
  The points on the graph are the new data. 
+
 During evaporation, the composition of the vapor varies continually.
  During evaporation, the composition of the vapor changes continually.
+
 The trucks have been purchased.
  The trucks have been bought.
+
 We have established three indoctrination centers.
  We have set up three indoctrination centers.
+
 The trailers greatly enhance mobility.
  The trucks greatly increase mobility.
+
 Rotate the handcrank fully clockwise.
  Turn the handcrank fully clockwise.
+
 Perform this test first.
  Do this test first.
+
 Everyone is aware of this problem.
  Everyone knows this problem. 
+
 The air conditioners will maintain the temperature.
  The air conditioners will keep the temperature. 
+
 A simple design also minimizes costs.
  A simple design also lowers costs.
