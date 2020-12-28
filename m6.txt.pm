b24cf0a0921112e68fd76bdc3cc9e16e8e021e46
rogarithm <rogarithm@gmail.com>
2020-12-28 18:05:14 +0900

    edit answers for m6 problems.
    manual pdfs for topics in relation gives words in real use, though not always.
    
    for the last problem, totally misunderstood what the problem means.
    edit from the bottom.
    
    for letter: googling, revising multiple time is my best at the moment, but is this
    okay if I work for real job?

--- m6.txt.pm
+++ m6.txt.pm
@@ -2,7 +2,7 @@
 
 1. 콘센트, 연장 케이블 및 어댑터에 과부하가 걸리지 않게 해야 한다. 상해나 감전 사고가 발생할 수 있다.
 
-as> Make sure that outlets, cables, and adapters not to be overloaded. there's a danger of injury or electric accident.
+as> Make sure that outlets, extension cables, and adapters not ◊uc{to be overloaded} to prevent injury or electrical shock incident.
 
 2. 전원 코드를 임의로 개조하거나 변경하지 말아야 한다.
 
@@ -10,16 +10,16 @@ as> Do not attempt to modify or change the power cord.
 
 3. 젖은 손으로 전원 플러그를 꽂거나 빼지 말아야 한다.
 
-as> It is dangerous to handle the power plug with wet hands.
+as> Do not handle the power plug when your hands are wet.
 
 4. 전원 플러그를 연속하여 꽂았다 빼지 말아야 한다.
 
-as> Power plug must not be ◊?{connected} and pulled ◊?{successively}.
+as> The power plug must not be plugged and unplugged repeatedly.
 
 5. 물이 샜을 경우 전원 플러그를 뺀 상태에서 물기를 완전히 제거해야 한다.
 
-as> In case of water leak, the water should be drained completely after the power plug get pulled off.
+as> In case of water leak, drain the water completely after the power plug unplugged.
 
 6. 루프 처리를 하지 않았다면 계기 후면의 광섬유 케이블을 사용하지 않는 경우, 보호 캡을 씌워야 한다.
 
-as> If loop setup hasn't been made, protect cap should be covered if you don't use light cable in ◊?{backside of meter}.
+as> If you didn't do loop setup, you need to put end cap on optical cable in backside of meter when you don't use it
