e023160ccccb21e35b5d81720593ac6ffc6b6b23
sehun-kim <rogarithm@gmail.com>
2021-01-21 21:06:40 +0900

    add feedback & compare of m12:
    - because checking determiner makes file messy, do this job in check-det
    branch.
    - keep working on making a method to organize information of determiner.
    - research pollen

---
+++ compare/cmp-m12.txt.pm
@@ -0,0 +1,40 @@
+diff --git a/manual/m12.txt b/manual/fm12.txt
+index ede8a77..31ea919 100644
+--- a/manual/m12.txt
++++ b/manual/fm12.txt
+@@ -1,28 +1,27 @@
+ 1. 레이저를 직접 쐬거나 반사된 빛을 쐬었을 때 눈에 부상을 입을 수 있다.
+ 
+-as> Exposure to direct beam or reflection from a laser may cause an eye injury.
+-◊xref[https://www.ehs.harvard.edu/sites/default/files/laser_safety_manual.pdf]{13}
++as> The laser may have sufficient power to cause an eye injury, both from the direct beam or from reflections.
+ 
+ 2. 제품의 포트(구멍)나 통풍구 등을 막지 말고, 이물질이 들어가지 않게 한다.
+ 
+-as> Do not close port (hole) or airflow hole of the product, and make sure foreign matter don't get inserted.
++as> Do not close the ports or ventilation holes and be sure not to insert foreign substances into them.
+ 
+ 3. 촛불, 담뱃불 등을 제품 위에 올려 두어선 안 된다.
+ 
+-as> Do not place lighted candles or burning cigarettes on the product.
++as> Do not place a candlelight or lighted cigarette on the product.
+ 
+ 4. 고장의 위험이 있으며, 올려놓은 물건이 떨어져서 다치거나 제품이 손상될 수 있다.
+ 
+-as> It may cause a product defect, and dropped material may cause injuries or damage to the product.
++as> It may cause a failure, injury resulting from a fall, or damage to the product.
+ 
+ 5. 온도가 상승하면 제품의 모양이 변형될 수 있다.
+ 
+-as> The increase in temperature may cause deformation of the product.
++as> Increase in temperature may cause a deformation of the product.
+ 
+ 6. 드라이브가 동작 중일 때는 Eject 버튼을 눌러서는 안 된다.
+ 
+-as> Do not push the Eject button while the drive is running.
++as> When the drive is running, do not press the Eject button.
+ 
+ 7. 연결선에 걸려 넘어지면 상해를 입을 수 있다.
+ 
+-as> Tripping over a connection cable may cause an injury.
++as> It may cause an injury due to fall over any cable.
