d5ace380799a7df0ea7815c0caf32eb24c1384a7
sehun-kim <rogarithm@gmail.com>
2021-02-04 01:02:54 +0900

    m22 initial writing

---
+++ manual/m22.txt.pm
@@ -0,0 +1,29 @@
+1. 진행 바가 표시된 상태에서 소리를 재생하세요.
+
+as> Play the sound while the progress bar is indicated.
+
+2. 클릭하면, 텍스트가 반전되어 보입니다.
+
+as> Click to highlight the text.
+
+3. 알파벳 순서로 정렬하세요.
+
+as> Sort in alphabetical order.
+
+4. 분해의 역순으로 부품을 조립하세요.
+
+as> Assemble the components in reverse order of disassembly
+
+◊link[https://www.sankyo-oilless.co.jp/english/products/files/SVSC-1006330.pdf]{source}
+
+5. 목록은 선택 항목에 의해 정렬됩니다. 
+
+as> The list is sorted by the selected item.
+
+6. 톱니는 엇배열되어 있습니다.
+
+as> The gear is arranged "arranged alternately".
+
+7. 이것들은 압축 파일로 저장하세요.
+
+as> Save these as zip file.
