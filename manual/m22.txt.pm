ba2a39bf6335f10233b3577a2e4005fd790f44a6
sehun-kim <rogarithm@gmail.com>
2021-02-04 02:09:05 +0900

    m22 style, article check. submit.

--- manual/m22.txt.pm
+++ manual/m22.txt.pm
@@ -1,6 +1,6 @@
 1. 진행 바가 표시된 상태에서 소리를 재생하세요.
 
-as> Play the sound while the progress bar is indicated.
+as> Play the sound while the progress bar is displayed.
 
 2. 클릭하면, 텍스트가 반전되어 보입니다.
 
@@ -12,17 +12,17 @@ as> Sort in alphabetical order.
 
 4. 분해의 역순으로 부품을 조립하세요.
 
-as> Assemble the components in reverse order of disassembly
+as> Assemble the components in reverse order of disassembly.
 
 ◊link[https://www.sankyo-oilless.co.jp/english/products/files/SVSC-1006330.pdf]{source}
 
-5. 목록은 선택 항목에 의해 정렬됩니다. 
+5. 목록은 선택 항목에 의해 정렬됩니다.
 
 as> The list is sorted by the selected item.
 
 6. 톱니는 엇배열되어 있습니다.
 
-as> The gear is arranged "arranged alternately".
+as> The cogs are alternately arranged.
 
 7. 이것들은 압축 파일로 저장하세요.
 
