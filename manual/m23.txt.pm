4c898cf2f69c77d742180118e73aca66f2cd7470
sehun-kim <rogarithm@gmail.com>
2021-02-05 00:45:43 +0900

    m23 refine writing. style and usage check.

--- manual/m23.txt.pm
+++ manual/m23.txt.pm
@@ -1,10 +1,10 @@
 1. 작업장을 청결한 상태로 유지하세요.
 
-as> Maintain the working area clean.
+as> Maintain the work area in a clean condition.
 
 2. 입력값을 저장하고 일치하는 값이 있으면 제시합니다. 
 
-as> Store the input to indicate if there's q value.
+as> Store an input to indicate if there's a matching value with the input.
 
 3. 스크롤해서 원하는 위치로 이동하세요.
 
@@ -12,16 +12,16 @@ as> Scroll to move where you want to go.
 
 4. 화면의 원 안에 눈이 들어오도록 해서 홍채를 인식시키세요.
 
-as> Let your eye come into the circle of screen to recognize the q.
+as> Let the eye caught inside circle of screen to detect iris.
 
 5. 손가락으로 좌우 스크롤을 해서 다른 화면으로 이동하세요.
 
-as> Scroll left and right with your finger to go other screen.
+as> Scroll left and right with your finger to move other screen.
 
 6. 패스워드로 허용되는 동일 인접 문자의 개수를 설정하세요.
 
-as> Configure allowed number of identical q character to be used as a password.
+as> Configure a number of adjacent identical "characters allowed in password".
 
 7. 서머타임을 적용하려면, 시계를 길게 누른 후 원하는 지역을 눌러서 선택하세요.
 
-as> To apply summertime, push and hold the clock and select the region you want.
+as> To turn on Daylight Saving Time, press and hold the chronometer and select the time zone you want.
