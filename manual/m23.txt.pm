83d0850eb620bc74af1b2c6c38fe5882d203ef73
sehun-kim <rogarithm@gmail.com>
2021-02-05 01:34:41 +0900

    m23 check article. comment on changes. submit.

--- manual/m23.txt.pm
+++ manual/m23.txt.pm
@@ -12,7 +12,9 @@ as> Scroll to the position where you want.
 
 4. 화면의 원 안에 눈이 들어오도록 해서 홍채를 인식시키세요.
 
-as> Recognize iris by fitting eyes inside the circle of screen.
+as> Recognize an iris by fitting eyes inside the circle of screen.
+
+◊comment{used "an" as this sentence is not to say a common knowledge, but to emphasize iris to be unspecified.}
 
 5. 손가락으로 좌우 스크롤을 해서 다른 화면으로 이동하세요.
 
@@ -20,10 +22,14 @@ as> Scroll left and right with your finger to move other screen.
 
 6. 패스워드로 허용되는 동일 인접 문자의 개수를 설정하세요.
 
-as> Set a number of identical adjacent "characters allowed in password".
+as> Set the number of identical adjacent characters allowed in password.
+
+◊commend{used "the" as the number is specified with following content}
 
 ◊link[https://www.ibm.com/support/knowledgecenter/SSLKT6_7.6.0/com.ibm.mt.doc/mbs_common/t_set_password_reqs.html]{number of identical adjacent characters}
 
 7. 서머타임을 적용하려면, 시계를 길게 누른 후 원하는 지역을 눌러서 선택하세요.
 
-as> To turn on Daylight Saving Time, press and hold the chronometer and select the time zone you want.
+as> To turn on Daylight Saving Time, press and hold the chronometer and select time zone you want.
+
+◊comment{"you" know what time zone you want, but not me."}
