8a81780c4051f4dde0cddca43e4efd26ed5d93c6
sehun-kim <rogarithm@gmail.com>
2021-02-05 01:22:47 +0900

    m23 check usage.

--- manual/m23.txt.pm
+++ manual/m23.txt.pm
@@ -8,11 +8,11 @@ as> Store an input to indicate if there's a matching value with the input.
 
 3. 스크롤해서 원하는 위치로 이동하세요.
 
-as> Scroll to move where you want to go.
+as> Scroll to the position where you want.
 
 4. 화면의 원 안에 눈이 들어오도록 해서 홍채를 인식시키세요.
 
-as> Let the eye caught inside circle of screen to detect iris.
+as> Recognize iris by fitting eyes inside the circle of screen.
 
 5. 손가락으로 좌우 스크롤을 해서 다른 화면으로 이동하세요.
 
@@ -20,7 +20,9 @@ as> Scroll left and right with your finger to move other screen.
 
 6. 패스워드로 허용되는 동일 인접 문자의 개수를 설정하세요.
 
-as> Configure a number of adjacent identical "characters allowed in password".
+as> Set a number of identical adjacent "characters allowed in password".
+
+◊link[https://www.ibm.com/support/knowledgecenter/SSLKT6_7.6.0/com.ibm.mt.doc/mbs_common/t_set_password_reqs.html]{number of identical adjacent characters}
 
 7. 서머타임을 적용하려면, 시계를 길게 누른 후 원하는 지역을 눌러서 선택하세요.
 
