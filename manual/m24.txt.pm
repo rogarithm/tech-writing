523753f5b2c27e9a068d0e00023416f2374ff737
rogarithm <rogarithm@gmail.com>
2021-02-05 16:46:27 +0900

    m24 check article. comment on changes. submit.

--- manual/m24.txt.pm
+++ manual/m24.txt.pm
@@ -1,6 +1,6 @@
 1. 그 카드는 40 km 거리까지 지원할 수 있습니다. 
 
-as> The card supports distances up to 40km.
+as> The card supports distances up to 40 km.
 
 2. 이 기기는 메모리 카드 용량을 128 GB까지 지원합니다.
 
@@ -8,15 +8,21 @@ as> This equipment supports memory card capacity up to 128 GB.
 
 3. 궤도는 북에서 남으로 정렬됩니다.
 
-as> The path is aligned to north to south.
+as> The track is lined up from north to south.
 
 4. 화면 잠금을 해제하기 위해 네 개 이상의 점을 연결한 패턴을 그리세요.
 
-as> Draw the pattern connecting "at least 4 dots" to unlock the screen lock.
+as> Draw pattern connecting at least 4 dots to unlock the screen lock.
+
+◊comment{using "the pattern" as this sentence describes about pattern to draw}
+
+◊comment{as it (may be) a smartphone manual, reader would know what is screen lock, so use "the screen lock"}
 
 5. 제품의 저장 공간을 확보하려면, 이 아이콘을 누르세요.
 
-as> Press the icon to reserve storage of the product.
+as> Press the icon to ensure storage of the product.
+
+◊comment{just "storage" as we don't know how much storage we need, and we can't count it.}
 
 6. 사용자의 잘못으로 인해 손실된 데이터에 대해 제조사는 책임지지 않습니다.
 
@@ -26,4 +32,8 @@ as> The provider makes no guarantee for data loss caused by user's fault.
 
 7. 확인하지 않은 알림이 있는 경우, 일정 간격으로 알리는 기능을 설정할 수 있습니다.
 
-as> When there's unchecked notification, you can set a function that sends notification at regular interval.
+as> When there's an unchecked notification, you can set a function that sends notifications at regular interval.
+
+◊comment{we can count how many notification, but we don't know exact number of it. I think "a" or "... notifications" would be proper one.}
+
+◊comment{not a common knowledge, so not zero article, but should be plural, so use "s" in the end.}
