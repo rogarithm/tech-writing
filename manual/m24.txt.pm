f06cce5cba804d9a59208d7cad0fd36288d0af97
rogarithm <rogarithm@gmail.com>
2021-02-05 16:29:55 +0900

    m24 check usage as far as I can

--- manual/m24.txt.pm
+++ manual/m24.txt.pm
@@ -4,11 +4,11 @@ as> The card supports distances up to 40km.
 
 2. 이 기기는 메모리 카드 용량을 128 GB까지 지원합니다.
 
-as> The equipment supports memory card capacity up to 128 GB.
+as> This equipment supports memory card capacity up to 128 GB.
 
 3. 궤도는 북에서 남으로 정렬됩니다.
 
-as> The orbit is aligned from the north to the south.
+as> The path is aligned to north to south.
 
 4. 화면 잠금을 해제하기 위해 네 개 이상의 점을 연결한 패턴을 그리세요.
 
@@ -16,14 +16,14 @@ as> Draw the pattern connecting "at least 4 dots" to unlock the screen lock.
 
 5. 제품의 저장 공간을 확보하려면, 이 아이콘을 누르세요.
 
-as> Tap the icon to q capacity of the product.
+as> Press the icon to reserve storage of the product.
 
 6. 사용자의 잘못으로 인해 손실된 데이터에 대해 제조사는 책임지지 않습니다.
 
-as> The product provider makes no guarantee for damaged data caused by user's fault.
+as> The provider makes no guarantee for data loss caused by user's fault.
 
 ◊link[http://www.blacksys.co.kr/admin/uploads_backup/20140314145520%7C%7CCW-100_ENG_130308.pdf]{user's fault}
 
 7. 확인하지 않은 알림이 있는 경우, 일정 간격으로 알리는 기능을 설정할 수 있습니다.
 
-as> When there's unchecked notification, you can set a function that notifies in regular interval.
+as> When there's unchecked notification, you can set a function that sends notification at regular interval.
