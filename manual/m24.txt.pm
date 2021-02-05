7f4596d810b2f1ae50dd22efb3138889a08b1991
sehun-kim <rogarithm@gmail.com>
2021-02-05 10:35:03 +0900

    m24 initial writing

---
+++ manual/m24.txt.pm
@@ -0,0 +1,27 @@
+1. 그 카드는 40 km 거리까지 지원할 수 있습니다. 
+
+as> The card supports q 40kmj
+
+2. 이 기기는 메모리 카드 용량을 128 GB까지 지원합니다.
+
+as> This equipment supports capacity of memory card up to 128 GB.
+
+3. 궤도는 북에서 남으로 정렬됩니다.
+
+as> The q is sorted from the north to the south.
+
+4. 화면 잠금을 해제하기 위해 네 개 이상의 점을 연결한 패턴을 그리세요.
+
+as> Draw the pattern connecting more than 4 dots to q the screen lock.
+
+5. 제품의 저장 공간을 확보하려면, 이 아이콘을 누르세요.
+
+as> Tap the icon to q capacity of the product.
+
+6. 사용자의 잘못으로 인해 손실된 데이터에 대해 제조사는 책임지지 않습니다.
+
+as> The provider isn't responsible for data loss from user's fault.
+
+7. 확인하지 않은 알림이 있는 경우, 일정 간격으로 알리는 기능을 설정할 수 있습니다.
+
+as> When there's a notification unchecked, you can set a q notifies in q q.
