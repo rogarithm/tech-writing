3b932e585c7a8f57185e8cf6ef80bdcd6ddff5f7
sehun-kim <rogarithm@gmail.com>
2021-02-05 10:47:26 +0900

    add form for style chapter

---
+++ others/book/style.html.pm
@@ -0,0 +1,21 @@
+#lang pollen
+
+◊title{style}
+
+◊section{}
+
+style is a contract to follow for consistent document. Then waht is the convention? It includes formatting, text labels, mechanics, punctuation, tone, use of graphics, word choice, etc.
+
+◊section{use correct grammar}
+
+◊section{use correct and consistent spelling}
+
+◊section{write with the appropriate tone}
+
+◊section{use an active style}
+
+◊section{use the appropriate mood}
+
+◊section{follow template designs and use boilerplate text}
+
+◊section{create and follow style guidelines}
