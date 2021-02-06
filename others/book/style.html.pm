b2b989d445816ac11f3923e244d86f2fd682ff56
sehun-kim <rogarithm@gmail.com>
2021-02-07 00:31:13 +0900

    write style chapter. start from indefinite pronouns (220p)

--- others/book/style.html.pm
+++ others/book/style.html.pm
@@ -4,9 +4,27 @@
 
 ◊section{}
 
-style is a contract to follow for consistent document. Then waht is the convention? It includes formatting, text labels, mechanics, punctuation, tone, use of graphics, word choice, etc.
+style is a contract to follow for consistent document. Then waht is the convention? It includes formatting, text labels, mechanics, punctuation, tone, use of graphics, word choice, etc. Style helps users understand the information.  
+
+What is style guidelines? These are style rules to ensure consistency across sets of information. Style guides choose in advance the elements of style, so ease writers' and editors' decision problem. Specific style guide could be developed to provide guide to elements that are not treated in general style guide, or to apply different convention for some reasons.
 
 ◊section{use correct grammar}
+
+Use correct grammar. If not, users will not credit your document, and your company. It's not always right to use formal grammar. If people use informal grammar more often, document written in formal grammar, though it's right, considered to be awkward. You need to be flexible. That is, consider your readers and their needs.
+
+common grammatical problems for writing technical information are as follows, with details of each one.
+
+◊bold{check for sentence fragments}
+
+Fragments are incomplete sentences. It's not always wrong to use them, if we allowed (or agreed) to use them. But don't use fragments within paragraphs.
+
+◊bold{correct pronoun problems}
+
+There's two cases: the relative pronouns (which and that) and indefinite pronouns.
+
+The relative pronouns are either restrictive or nonrestrictive. Nonrestrictive clauses do not specify its range of meaning, so even when they are dropped out, there's no loss in meaning. Nonrestrictive pronouns are preceded by comma. Many style guides recommend using that with restrictive clauses, and which (plus commas) with nonrestrictive clauses.
+
+◊start-here
 
 ◊section{use correct and consistent spelling}
 
