4ddc1c6681cc7ba4383211af74c8ad286b4f3dcc
rogarithm <rogarithm@gmail.com>
2021-02-06 17:50:13 +0900

    m25 initial writing

---
+++ manual/m25.txt.pm
@@ -0,0 +1,27 @@
+1. 제품이 꺼진 경우, 전화를 다른 번호로 착신 전환하도록 설정하세요.
+
+as> When the product turned off, set the phone to switch other number.
+
+2. 다음 메시지를 포함한 대기 입력창이 나타납니다.
+
+as> Pause input screen including the message shows.
+
+3. 설명이 말풍선 도움말로 표시됩니다.
+
+as> The description is specified as q help.
+
+4. 누를 때마다 네 개의 표시 모드가 번갈아가며 나타납니다.
+
+as> The four display mode shows in turn at each tap.
+
+5. 그것은 규정한 수만큼 데이터 로그를 표시할 수 있습니다.
+
+as> It can show data logs for the specified number.
+
+6. 그것은 소수점 셋째 자리까지 값을 표시합니다.
+
+as> It shows the value to third location after decimal point.
+
+7. 아이가 길을 잃거나 행방불명되는 일이 없도록 방지하세요.
+
+as> Make sure child don't get lost or q.
