8566d676b2009d39d72f61bd2be95fa7f5351976
sehun-kim <rogarithm@gmail.com>
2021-02-07 23:08:09 +0900

    m25 check usage (note with "".)

--- manual/m25.txt.pm
+++ manual/m25.txt.pm
@@ -1,14 +1,16 @@
 1. 제품이 꺼진 경우, 전화를 다른 번호로 착신 전환하도록 설정하세요.
 
-as> When the product turned off, set the phone to "forward call" to other number.
+as> "When the product is turned off", set up the phone to forward to another number.
+
+◊link[https://cdn-media.vtechphones.com/media/p/document2/products/%7BD2E7336F-69F6-4387-B9CC-0DDE6AE56690%7D/VTech_VSP725_User_Guide_11142014.pdf]{forward to another number}
 
 2. 다음 메시지를 포함한 대기 입력창이 나타납니다.
 
-as> Pause(q) input screen shows including the message.
+as> "Input wait window" shows up including the following message.
 
 3. 설명이 말풍선 도움말로 표시됩니다.
 
-as> The explanation is presented as word balloon help.
+as> The description appears as "balloon help text".
 
 4. 누를 때마다 네 개의 표시 모드가 번갈아가며 나타납니다.
 
@@ -16,12 +18,12 @@ as> The four display mode "appear in turn" "with each tap".
 
 5. 그것은 규정한 수만큼 데이터 로그를 표시할 수 있습니다.
 
-as> It can "indicate" data logs (log data?) for the "stipulated" number.
+as> It displays data logs for the specified number.
 
 6. 그것은 소수점 셋째 자리까지 값을 표시합니다.
 
-as> It "displays" value up to thid decimal digit from the decimal point
+as> It displays a value up to 3 decimal places.
 
 7. 아이가 길을 잃거나 행방불명되는 일이 없도록 방지하세요.
 
-as> Make sure child don't get lost or q.
+as> Prevent a child from getting lost or from going missing.
