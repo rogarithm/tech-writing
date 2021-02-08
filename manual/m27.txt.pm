a29c609a25708c42d49de599a9df6e2cd5bc6536
sehun-kim <rogarithm@gmail.com>
2021-02-09 02:58:55 +0900

    m27 check usage wip.

--- manual/m27.txt.pm
+++ manual/m27.txt.pm
@@ -1,22 +1,30 @@
 1. 현재 날짜의 3개월 앞과 뒤의 범위 내 값을 취해야 합니다.
 
-as> Choose a value from 3 months before to 3 months later of the current date.
+as'''> select a value between the date before three months the current date and after three months the current date.
+
+as''>select a value with its range between before 3 months and after 3 months of the current date.
+
+as'> select a value with its range between two values: 3 months before the current date, and 3 months after the current date.
+
+as> Choose a value with its date range between date 3 months before and date 3 month after the current date.
 
 2. 임의로 제품을 개조하거나 조작해서는 안 됩니다.
 
-as> Do not arbitrarily manipulate or operate the product.
+as> Do not arbitrarily modify or operate the product.
 
 3. 자세한 내용은 '2.1 주요 특징'을 참고하세요.
 
-as> For the detail, refer to '2.1 q characteristcs.'
+as> For details, refer to section 2.1 the Main Characteristcs.
+
+◊comment{can we count detail? examples for using quotes}
 
 4. 사용자 매뉴얼을 참조하세요.
 
-as> Refer to user manual.
+as> Refer to the user manual.
 
 5. ??? 웹사이트를 참조하세요.
 
-as> Refer to ??? website.
+as> Refer to the ??? website.
 
 6. 24페이지를 참고하세요.
 
@@ -24,4 +32,6 @@ as> Refer to page 24.
 
 7. 카테고리별로 애플리케이션을 검색하거나 키워드로 검색하세요.
 
-as> Search applications with categories or keywords.
+as> Browse applications by category or keyword.
+
+◊comment{browse seems more realistic usage than search or find. why category and keyword, not categories and keywords?}
