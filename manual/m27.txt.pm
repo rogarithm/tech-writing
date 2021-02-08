9aedadf8414bda022b754a213ed20d8f4d2b3989
sehun-kim <rogarithm@gmail.com>
2021-02-09 02:57:51 +0900

    m27 initial writing

---
+++ manual/m27.txt.pm
@@ -0,0 +1,27 @@
+1. 현재 날짜의 3개월 앞과 뒤의 범위 내 값을 취해야 합니다.
+
+as> Choose a value from 3 months before to 3 months later of the current date.
+
+2. 임의로 제품을 개조하거나 조작해서는 안 됩니다.
+
+as> Do not arbitrarily manipulate or operate the product.
+
+3. 자세한 내용은 '2.1 주요 특징'을 참고하세요.
+
+as> For the detail, refer to '2.1 q characteristcs.'
+
+4. 사용자 매뉴얼을 참조하세요.
+
+as> Refer to user manual.
+
+5. ??? 웹사이트를 참조하세요.
+
+as> Refer to ??? website.
+
+6. 24페이지를 참고하세요.
+
+as> Refer to page 24.
+
+7. 카테고리별로 애플리케이션을 검색하거나 키워드로 검색하세요.
+
+as> Search applications with categories or keywords.
