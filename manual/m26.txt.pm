62b64eafa9b27c9bdd35ead981f602781774b586
rogarithm <rogarithm@gmail.com>
2021-02-08 15:42:47 +0900

    m26 check usage

--- manual/m26.txt.pm
+++ manual/m26.txt.pm
@@ -4,25 +4,24 @@ as> Place harmful material away from children so that they don't touch it.
 
 2. 6개 메뉴를 사용할 수 있습니다. 
 
-as> Six menus available.
+as> There are six menus available.
 
 3. 4자리 비밀번호를 입력하세요.
 
-as> Type 4 q password.
+as> "Enter" four digit password.
 
 4. 천의 자리수에서 값을 올림하세요. 
 
-as> Round up the value in thousand place.
+as> Round up the value to the nearest ten thousand.
 
 5. 용제 1에 물 10의 비율로 섞으세요.
 
-as> Mix q and water with 1:10 ratio.
+as> Mix solvent and water in the ratio of one to ten.
 
 6. 프로세서의 백분의 1만큼씩 증가하여 변합니다.
 
-as> It changes by one hundredth of the processor.
+as> It "increases by one hundredth of the processor".
 
 7. 그것은 3 % 이상 다르지 않습니다.
 
-as> It's not different more than 3 %.
-not more different
+as> It's not different more than 3%.
