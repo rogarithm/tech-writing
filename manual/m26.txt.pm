5be4c0415e644cdb838b0317f099357523c849ca
sehun-kim <rogarithm@gmail.com>
2021-02-08 12:38:20 +0900

    m26 initial writing

---
+++ manual/m26.txt.pm
@@ -0,0 +1,31 @@
+1. 제품이 꺼진 경우, 전화를 다른 번호로 착신 전환하도록 설정하세요.
+
+as> When the product is turned off, set up to forward a call to another number.
+
+◊link[https://cdn-media.vtechphones.com/media/p/document2/products/%7BD2E7336F-69F6-4387-B9CC-0DDE6AE56690%7D/VTech_VSP725_User_Guide_11142014.pdf]{forward to another number}
+
+2. 다음 메시지를 포함한 대기 입력창이 나타납니다.
+
+as> Input wait window appears including the following message.
+
+3. 설명이 말풍선 도움말로 표시됩니다.
+
+as> The description appears as "balloon help".
+
+4. 누를 때마다 네 개의 표시 모드가 번갈아가며 나타납니다.
+
+as> The four display modes appear in turn with each tap.
+
+5. 그것은 규정한 수만큼 데이터 로그를 표시할 수 있습니다.
+
+as> It displays data logs for a specified number.
+
+◊comment{"a specified" as the number is not chosen yet, and we don't know it yet.}
+
+6. 그것은 소수점 셋째 자리까지 값을 표시합니다.
+
+as> It displays a value up to 3 decimal places.
+
+7. 아이가 길을 잃거나 행방불명되는 일이 없도록 방지하세요.
+
+as> Prevent a child from getting lost or from going missing.
