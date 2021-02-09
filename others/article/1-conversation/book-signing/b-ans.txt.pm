2c676e7548265e80471aaa142f286dae0978fd5d
sehun-kim <rogarithm@gmail.com>
2021-02-09 22:15:33 +0900

    add article-check files for article book:
    - refine its structure
    - why I use this:
    
     The book about article I've been reading starts each section with
     article exercise that I can check its solution right next page.
    
     The problem is, to check the difference btw my answer and solution
     is quite a burden. I may not remember what article I've chosen in certain
     position. I could write all answers in a paper, but it'll get long time.
    
     So I try this. Checking diff with git diff!

---
+++ others/article/1-conversation/book-signing/b-ans.txt.pm
@@ -0,0 +1,17 @@
+1-jennifer> z
+
+1-james> z t z
+
+2-jennifer> t t
+
+2-james> t t
+
+3-jennifer> z
+
+3-james> t z
+
+4-jennifer> t z
+
+4-james> t
+
+5-jennifer> z
---
+++ others/article/1-conversation/book-signing/my-ans.txt.pm
@@ -0,0 +1,17 @@
+1-jennifer> z
+
+1-james> z t z
+
+2-jennifer> t t
+
+2-james> t t
+
+3-jennifer> a
+
+3-james> t t
+
+4-jennifer> t t
+
+4-james> t
+
+5-jennifer> z
