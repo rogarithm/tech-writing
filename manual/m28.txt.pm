0aa68935c44c73bf916c2ec3c94bcb42a3db1503
sehun-kim <rogarithm@gmail.com>
2021-02-16 15:08:23 +0900

    m28 check usage

--- manual/m28.txt.pm
+++ manual/m28.txt.pm
@@ -1,31 +1,31 @@
 1. 그림 4에서 보여지는 것처럼 바닥이 중량을 지탱할 수 있다는 뜻입니다.
 
-as> As you can see in figure 4, the floor can q the weight.
+as> As you can see in the figure 4, the floor can support the weight.
 
 2. 카드를 살짝 눌러서 트레이에 끼워 고정하세요.
 
-as> Fasten it by inserting the card to the tray with the card slightly pushed.
+as> Fasten the card to the tray by slightly pushing the card to the tray.
 
 3. 단자에 과도한 힘을 가하지 마세요.
 
-as> Do not put extreme load to the port.
+as> Do not put excessive pressure on the socket.
 
 4. 스위치를 왼쪽 끝까지 미세요.
 
-as> Push the switch to the left q.
+as> Slide the switch to the left end.
 
 5. 그것을 카드리더에 긁어야 합니다. 
 
-as> Scratch it to the card reader.
+as> You need to swipe it "in" the card reader.
 
 6. 그것은 최대값보다 26 % 큽니다. 
 
-as> It is 26 % bigger than the maximum value
+as> It is 26% larger than the maximum value
 
 7. 원하는 횟수만큼 버튼을 누르세요.
 
-as> Push the button for the number of time you want.
+as> Push the button as many times as you want.
 
 8. 그것은 오른쪽 마우스 버튼과 동일한 동작을 합니다.
 
-as> It works the same with the right button of mouse.
+as> It works the same as right button of mouse.
