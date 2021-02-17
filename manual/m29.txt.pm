ed153f69573c364b382a183f1bd490e954c9e1ec
sehun-kim <rogarithm@gmail.com>
2021-02-17 14:08:45 +0900

    m29 initial writing

---
+++ manual/m29.txt.pm
@@ -0,0 +1,31 @@
+1. 그것은 95 %의 정확성과 0.98 %의 오차 범위에 있습니다. 
+
+as> It's in the range of 95 percent of precision and 0.98 percent of error scope.
+
+2. 그것은 수평면으로 잘랐을 때 타원형에 가깝습니다. 
+
+as> It's near oval form when it's cut in horizontal surface.
+
+3. 페달의 움직임이 부드럽게 느껴지지 않습니다. 
+
+as> The movement of the pedal isn't feel soft.
+
+4. 대전용 롤러가 감광 드럼의 표면 전체를 정전기로 대전시킵니다.
+
+as> The roller for q q the whole surface of the drum with q.
+
+5. 캠코더는 8메가 픽셀로 사진을 찍습니다. 
+
+as> The camcoder take a picture with 8 mega pixel.
+
+6. 음악이 4분의 3박자로 연주됩니다. 
+
+as> The music is played in 4 by 3.
+
+7. 그것은 다른 신호에 우선합니다. 
+
+as> It precedes other signal.
+
+8. 그것은 파이프와 수직입니다.
+
+as> It's q with the pipe.
