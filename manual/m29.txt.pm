3b5f71df601c6b4ce928c9e48e3a7e79a0a4927f
sehun-kim <rogarithm@gmail.com>
2021-02-17 16:57:06 +0900

    typo. remove double quotes.

--- manual/m29.txt.pm
+++ manual/m29.txt.pm
@@ -8,17 +8,17 @@ as> It's close to the oval shape when it's cut by a horizontal plane.
 
 3. 페달의 움직임이 부드럽게 느껴지지 않습니다. 
 
-as> The movement of the pedal doesn't feel "smooth".
+as> The movement of the pedal doesn't feel smooth.
 
 4. 대전용 롤러가 감광 드럼의 표면 전체를 정전기로 대전시킵니다.
 
-as> The charging roller charges "the whole surface of the exposure drum" with electrostatic.
+as> The charging roller charges the whole surface of the exposure drum with electrostatic.
 
-◊comment{search result matched "" without the word whole.}
+◊comment{search result matched  without the word whole.}
 
 5. 캠코더는 8메가 픽셀로 사진을 찍습니다. 
 
-as> The camcoder "captures 8 megapixel photos."
+as> The camcorder captures 8 megapixel photos.
 
 6. 음악이 4분의 3박자로 연주됩니다. 
 
