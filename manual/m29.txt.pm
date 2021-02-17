a349c879f8070a39daefce1b167eba07cbb0c953
sehun-kim <rogarithm@gmail.com>
2021-02-17 16:37:53 +0900

    m29 check usage and article, submit:
      Searched & checked words are enclosed by "".
      Ask about using articles in the case like:
      "the whole surface of the exposure drum."

--- manual/m29.txt.pm
+++ manual/m29.txt.pm
@@ -1,31 +1,33 @@
 1. 그것은 95 %의 정확성과 0.98 %의 오차 범위에 있습니다. 
 
-as> It's in the range of 95 percent of precision and 0.98 percent of error.
+as> It has 95 percent accuracy and in the range of 0.98 percent error.
 
 2. 그것은 수평면으로 잘랐을 때 타원형에 가깝습니다. 
 
-as> It's close to the oval shape when it's cut by the horizontal plane.
+as> It's close to the oval shape when it's cut by a horizontal plane.
 
 3. 페달의 움직임이 부드럽게 느껴지지 않습니다. 
 
-as> The movement of the pedal isn't feel soft.
+as> The movement of the pedal doesn't feel "smooth".
 
 4. 대전용 롤러가 감광 드럼의 표면 전체를 정전기로 대전시킵니다.
 
-as> The "charging roller" "charges" the whole surface of the "exposure" drum with electrostatic.
+as> The charging roller charges "the whole surface of the exposure drum" with electrostatic.
+
+◊comment{search result matched "" without the word whole.}
 
 5. 캠코더는 8메가 픽셀로 사진을 찍습니다. 
 
-as> The camcoder take a picture with 8 mega pixel.
+as> The camcoder "captures 8 megapixel photos."
 
 6. 음악이 4분의 3박자로 연주됩니다. 
 
-as> The music is played in "three-four time".
+as> The music is played in three-four time.
 
 7. 그것은 다른 신호에 우선합니다. 
 
-as> It precedes other signal.
+as> It takes precedence over other signals.
 
 8. 그것은 파이프와 수직입니다.
 
-as> It's "perpendicular to" the pipe.
+as> It's perpendicular to the pipe.
