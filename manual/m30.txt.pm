85d9346f787f751a2da4bd2bb0e6bf1512dc746a
sehun-kim <rogarithm@gmail.com>
2021-02-18 13:40:52 +0900

    m30 check usage. note "ok" where the translation done.

--- manual/m30.txt.pm
+++ manual/m30.txt.pm
@@ -1,33 +1,33 @@
 1. 견인 트럭이 차를 끌어 올릴 수 있습니다. 
 
-as> The "tow truck" can lift up a car.
+as> The tow truck can lift up a car.
 
-2. 배터리는 직렬로 연결되어 있습니다. 
+ok 2. 배터리는 직렬로 연결되어 있습니다. 
 
-as> The batteries are connected "in series".
+as> The batteries are connected in series.
 
-3. 그것은 복사 방지가 되어 있습니다.
+ok 3. 그것은 복사 방지가 되어 있습니다.
 
-as> The product is qed not to be copied.
+as> The product is copy protected.
 
-"copy protection"
+ok 4. 지역 대리점과 상담하세요.
 
-4. 지역 대리점과 상담하세요.
-
-as> Access the local "agency".
+as> "Consult" the local agency.
 
 5. 수학공식에 따라 대입하세요.
 
-as> "Substitute" according to the math "formula".
+as> Substitute according to the math formula.
 
-6. 서버를 정상적으로 셧다운시키십시오.
+ok 6. 서버를 정상적으로 셧다운시키십시오.
 
 as> Shutdown the server normally.
 
-7. 미지근한 물에 중성세제를 희석시키십시오.
+ok 7. 미지근한 물에 중성세제를 희석시키십시오.
 
-as> "Dilute" the "neutral detergent" in the "tepid" water.
+as> Dilute neutral detergent with warm water.
 
-8. 소수점 셋째 자리에서 1.523을 반올림하세요.
+◊link[https://www.muji.com.hk/en/maintenance.php]{maintenance of wooden furniture}
 
-as> Round up 1.523 "to three decimal places".
+ok 8. 소수점 셋째 자리에서 1.523을 반올림하세요.
+
+as> Round up 1.523 to two decimal places.
