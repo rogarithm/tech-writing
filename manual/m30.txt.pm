acdaf970a883402ab0f400133a2f1752e7b764b8
sehun-kim <rogarithm@gmail.com>
2021-02-18 13:11:58 +0900

    m30 search words in dictionary

--- manual/m30.txt.pm
+++ manual/m30.txt.pm
@@ -1,22 +1,24 @@
 1. 견인 트럭이 차를 끌어 올릴 수 있습니다. 
 
-as> The q truck can lift up a car.
+as> The "tow truck" can lift up a car.
 
 2. 배터리는 직렬로 연결되어 있습니다. 
 
-as> The batteries are connected in q.
+as> The batteries are connected "in series".
 
 3. 그것은 복사 방지가 되어 있습니다.
 
 as> The product is qed not to be copied.
 
+"copy protection"
+
 4. 지역 대리점과 상담하세요.
 
-as> Access the local q.
+as> Access the local "agency".
 
 5. 수학공식에 따라 대입하세요.
 
-as> q according to the math theorem.
+as> "Substitute" according to the math "formula".
 
 6. 서버를 정상적으로 셧다운시키십시오.
 
@@ -24,8 +26,8 @@ as> Shutdown the server normally.
 
 7. 미지근한 물에 중성세제를 희석시키십시오.
 
-as> q the q q in the q water.
+as> "Dilute" the "neutral detergent" in the "tepid" water.
 
 8. 소수점 셋째 자리에서 1.523을 반올림하세요.
 
-as> Round up 1.523 in q place after decimal point.
+as> Round up 1.523 "to three decimal places".
