9862fe31a15a732b7f5a4bd431fd5bcc810cfc5e
sehun-kim <rogarithm@gmail.com>
2021-02-18 23:13:42 +0900

    m31 check usage. note "ok" where the translation done.

--- manual/m31.txt.pm
+++ manual/m31.txt.pm
@@ -1,31 +1,31 @@
-1. 의자를 접으세요.
+ok 1. 의자를 접으세요.
 
-as> "Fold" the chair.
+as> Fold the chair.
 
-2. 렌치로 적당히 돌려서 피팅을 조이세요.
+ok 2. 렌치로 적당히 돌려서 피팅을 조이세요.
 
-as> "Tighten" the fitting by "turning" the wrench moderately.
+as> Tighten the fitting "by ◊ignore{moderatelay} turning it with a wrench".
 
 3. 디스크가 흔들리는지 점검하세요.
 
-as> Check if the disk "shakes".
+as> Check if the disk shakes.
 
 4. 조인트를 돌려서 20밀리미터의 유격을 얻으세요.
 
-as> Get 20 milimeter of "spacing" by "turning" the joint.
+as> Get 20 milimeter spacing by turning the joint.
 
-5. 식의 양변에 로그를 취하세요.
+ok 5. 식의 양변에 로그를 취하세요.
 
-as> Apply log on both side of the equation.
+as> Apply log on both sides of the equation.
 
 6. 서로 다른 속도에서 대상체를 5회 왕복시키십시오.
 
-as> "Shuttle" the object 5 times in a different speed.
+as> Shuttle the object 5 times in different speeds.
 
 7. 체중이 실리는 방향으로 무게 중심을 유지하세요.
 
-as> "Maintain" the "center of gravity" in the direction where "your weight put on".
+as> Maintain the center of gravity in the direction where body weight put on.
 
-8. 그것이 수평 위치로 놓여 있는지 확인하세요.
+ok 8. 그것이 수평 위치로 놓여 있는지 확인하세요.
 
-as> Check if it is located in horizontal position.
+as> Check if it is located in a horizontal position.
