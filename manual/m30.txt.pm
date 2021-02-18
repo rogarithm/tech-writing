c56a2af46ee5a6f05cb589d060937bc5d0a514a8
sehun-kim <rogarithm@gmail.com>
2021-02-18 14:25:14 +0900

    m30 check usage and article, submit.

--- manual/m30.txt.pm
+++ manual/m30.txt.pm
@@ -1,33 +1,35 @@
 1. 견인 트럭이 차를 끌어 올릴 수 있습니다. 
 
-as> The tow truck can lift up a car.
+as> The tow truck can lift a car.
 
-ok 2. 배터리는 직렬로 연결되어 있습니다. 
+◊commend{From the dictionary, the word "lift" has a meaning "up" in it}
+
+2. 배터리는 직렬로 연결되어 있습니다. 
 
 as> The batteries are connected in series.
 
-ok 3. 그것은 복사 방지가 되어 있습니다.
+3. 그것은 복사 방지가 되어 있습니다.
 
 as> The product is copy protected.
 
-ok 4. 지역 대리점과 상담하세요.
+4. 지역 대리점과 상담하세요.
 
-as> "Consult" the local agency.
+as> Consult the local agency.
 
 5. 수학공식에 따라 대입하세요.
 
-as> Substitute according to the math formula.
+as> Substitute using the math formula.
 
-ok 6. 서버를 정상적으로 셧다운시키십시오.
+6. 서버를 정상적으로 셧다운시키십시오.
 
 as> Shutdown the server normally.
 
-ok 7. 미지근한 물에 중성세제를 희석시키십시오.
+7. 미지근한 물에 중성세제를 희석시키십시오.
 
 as> Dilute neutral detergent with warm water.
 
 ◊link[https://www.muji.com.hk/en/maintenance.php]{maintenance of wooden furniture}
 
-ok 8. 소수점 셋째 자리에서 1.523을 반올림하세요.
+8. 소수점 셋째 자리에서 1.523을 반올림하세요.
 
 as> Round up 1.523 to two decimal places.
