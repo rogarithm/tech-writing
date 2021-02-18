558aaf47efcc4f2e0412de37ffa3e6bb4ab82849
sehun-kim <rogarithm@gmail.com>
2021-02-18 12:57:13 +0900

    m30 initial writing

---
+++ manual/m30.txt.pm
@@ -0,0 +1,31 @@
+1. 견인 트럭이 차를 끌어 올릴 수 있습니다. 
+
+as> The q truck can lift up a car.
+
+2. 배터리는 직렬로 연결되어 있습니다. 
+
+as> The batteries are connected in q.
+
+3. 그것은 복사 방지가 되어 있습니다.
+
+as> The product is qed not to be copied.
+
+4. 지역 대리점과 상담하세요.
+
+as> Access the local q.
+
+5. 수학공식에 따라 대입하세요.
+
+as> q according to the math theorem.
+
+6. 서버를 정상적으로 셧다운시키십시오.
+
+as> Shutdown the server normally.
+
+7. 미지근한 물에 중성세제를 희석시키십시오.
+
+as> q the q q in the q water.
+
+8. 소수점 셋째 자리에서 1.523을 반올림하세요.
+
+as> Round up 1.523 in q place after decimal point.
