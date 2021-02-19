c08c01bd421a0b41eaa3db0a61ee9d394cbbdd11
sehun-kim <rogarithm@gmail.com>
2021-02-19 11:28:28 +0900

    m32 initial writing

---
+++ manual/m32.txt.pm
@@ -0,0 +1,31 @@
+1. 온도 변화가 심한 곳에서 제품을 사용하지 마세요.
+
+as> Do not use the product where temperature changes q.
+
+2. 제품에 대한 정보를 두서없이 설명하면 사용자의 혼란을 야기시킬 수 있습니다.
+
+as> Users can be confused when explaining the product information without q.
+
+3. 전원 코드가 밟히지 않도록 배선해야 합니다.
+
+as> Make sure q with the power cord not get caught.
+
+4. 너트가 느슨해지면 자전거 바퀴가 빠질 수 있습니다.
+
+as> The bicycle q can be dropped off when the nut get loosen.
+
+5. 육지가 배의 정북에 위치해 있어야 합니다. 
+
+as> The q should be present on the right north of the ship.
+
+6. 모든 정비는 전문가에게 맡기세요.
+
+as> q all maintenance to the expert.
+
+7. 가치 있는 수정은 탄소가 엄청난 열과 압력을 받을 때 형성됩니다.
+
+as> Valuable crystal is made when carbon get extreme heat and pressure.
+
+8. 지명이 라틴어 알파벳으로 음역되었습니다.
+
+as> q is q with latin alphabet.
