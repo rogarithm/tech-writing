70138fc1fb9cd3ac9d6b4bbd1131b754648e896b
sehun-kim <rogarithm@gmail.com>
2021-02-19 12:54:45 +0900

    m32 check usage and article, submit.

--- manual/m32.txt.pm
+++ manual/m32.txt.pm
@@ -1,18 +1,18 @@
-ok 1. 온도 변화가 심한 곳에서 제품을 사용하지 마세요.
+1. 온도 변화가 심한 곳에서 제품을 사용하지 마세요.
 
-as> Do not use the product in a place where temperature changes "strongly".
+as> Do not use the product in a place where temperature changes strongly.
 
 2. 제품에 대한 정보를 두서없이 설명하면 사용자의 혼란을 야기시킬 수 있습니다.
 
 as> Rambling on the product information could make users get confused.
 
-ok 3. 전원 코드가 밟히지 않도록 배선해야 합니다.
+3. 전원 코드가 밟히지 않도록 배선해야 합니다.
 
 as> Wire carefully so that the power cord is not stepped on.
 
-◊commend{search with "the power cord * stepped"}
+◊comment{search with "the power cord * stepped"}
 
-ok 4. 너트가 느슨해지면 자전거 바퀴가 빠질 수 있습니다.
+4. 너트가 느슨해지면 자전거 바퀴가 빠질 수 있습니다.
 
 as> The bicycle wheel may come out when a nut gets loose.
 
@@ -20,14 +20,16 @@ as> The bicycle wheel may come out when a nut gets loose.
 
 as> Land should be placed on dew north of the ship.
 
-ok 6. 모든 정비는 전문가에게 맡기세요.
+6. 모든 정비는 전문가에게 맡기세요.
 
-as> Assign all maintenance to the expert.
+as> Assign all maintenance to the experts.
+
+◊comment{seems pointing a group of experts}
 
 7. 가치 있는 수정은 탄소가 엄청난 열과 압력을 받을 때 형성됩니다.
 
-as> Valuable crystal is made when carbon get extreme heat and pressure.
+as> Valuable crystal is made when carbon gets extreme heat and pressure.
 
-ok 8. 지명이 라틴어 알파벳으로 음역되었습니다.
+8. 지명이 라틴어 알파벳으로 음역되었습니다.
 
 as> The place name was transliterated into latin alphabet.
