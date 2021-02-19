1b4c89f958f5cd32e9e205855d834f8e77d68241
sehun-kim <rogarithm@gmail.com>
2021-02-19 11:45:05 +0900

    m32 search words in dictionary, check usage.

--- manual/m32.txt.pm
+++ manual/m32.txt.pm
@@ -1,26 +1,26 @@
 1. 온도 변화가 심한 곳에서 제품을 사용하지 마세요.
 
-as> Do not use the product where temperature changes q.
+as> Do not use the product where temperature changes "heavily".
 
 2. 제품에 대한 정보를 두서없이 설명하면 사용자의 혼란을 야기시킬 수 있습니다.
 
-as> Users can be confused when explaining the product information without q.
+as> Users can be confused when "ramble on" explaining the product information.
 
 3. 전원 코드가 밟히지 않도록 배선해야 합니다.
 
-as> Make sure q with the power cord not get caught.
+as> Make sure "to wire" with the power cord not get caught.
 
 4. 너트가 느슨해지면 자전거 바퀴가 빠질 수 있습니다.
 
-as> The bicycle q can be dropped off when the nut get loosen.
+as> The bicycle wheel may "come out" when the nut get loosen.
 
 5. 육지가 배의 정북에 위치해 있어야 합니다. 
 
-as> The q should be present on the right north of the ship.
+as> The "land" should be on the "dew north" of the ship.
 
 6. 모든 정비는 전문가에게 맡기세요.
 
-as> q all maintenance to the expert.
+as> "Assign" all maintenance to the expert.
 
 7. 가치 있는 수정은 탄소가 엄청난 열과 압력을 받을 때 형성됩니다.
 
@@ -28,4 +28,4 @@ as> Valuable crystal is made when carbon get extreme heat and pressure.
 
 8. 지명이 라틴어 알파벳으로 음역되었습니다.
 
-as> q is q with latin alphabet.
+as> q "was transliterated" "into" latin alphabet.
