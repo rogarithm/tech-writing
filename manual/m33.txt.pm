8217f32d66b51ca09de84c289919715fb33e7ce6
sehun-kim <rogarithm@gmail.com>
2021-02-21 15:26:37 +0900

    m33 check usage

--- manual/m33.txt.pm
+++ manual/m33.txt.pm
@@ -1,31 +1,37 @@
 1. 바깥쪽 표피는 멜라닌을 함유한 거친 보호층입니다.
 
-as> "Outer epidermis" is a harsh "protective layer"  containing melanin.
+as> Outer epidermis is a harsh protective layer containing melanin.
 
 2. 인공기관은 잃어버린 신체를 대신하는 팔, 다리, 치아와 같은 인공 신체를 말합니다.
 
-as> Artificial "organ" is an artificial body like arm, legs, tooth that "replaces" the body.
+as> Artificial organ is an artificial body "such as" arm, leg, tooth that replaces lost natural organ.
+
+◊comment{are the arm, leg, tooth plural?}
 
 3. CPU는 세라믹이나 플라스틱 용기에 싸여 있습니다.
 
-as> CPU is wrapped by a ceramic or plastic container.
+as> CPU is "wrapped up" with a ceramic or plastic container.
 
 4. 그 약은 두 번의 임상시험에서 78 %의 치료율을 보였습니다.
 
-as> The medicine showed 78% of cure rate in clinical test that performed twice.
+as> The medicine showed 78% of cure rate in the clinical tests that were performed twice.
+
+◊link[https://www.donga.com/en/article/all/20010706/207910/1/Liver-Cancer-Medicine-Made-In-Korea-Approved]{source}
 
 5. 소프트웨어 스택은 어떤 결과를 도출하거나 공통 목표를 달성하기 위해 함께 동작하는 프로그램의 그룹입니다.
 
-as> Software stack is a group of programs that runs together to "draw" a result or to accomplish a common "goal".
+as> Software stack is a group of programs that work together to deduce a result or to accomplish a common goal.
 
 6. 그것들은 서로 평행인 채로 위치해 있습니다.
 
-as> These are located as "parallel" each other.
+as> These are located "in" parallel each other.
 
 7. 안장 기둥은 개인의 몸 비율에 맞출 수 있습니다.
 
-as> "Saddle pillar" can be fitted according to the body "rate" of individual.
+as> Seatpost can be set according to the body proportions of individuals.
+
+◊comment{Synonyms for saddle pillar: seatpost, seatpin, saddlepole, saddle pin}
 
 8. 해당 서비스는 모바일 운영체제 기반 앱에 광고를 제공할 수 있습니다.
 
-as> The service can provide an advertisement to an app based on mobile operating system.
+as> The "following" service may provide an advertisement to an app based on mobile operating system.
