0b87935ca2716192aec69bd137a40bb18e6987ce
sehun-kim <rogarithm@gmail.com>
2021-02-21 14:45:24 +0900

    m33 search words in dictionary

--- manual/m33.txt.pm
+++ manual/m33.txt.pm
@@ -1,10 +1,10 @@
 1. 바깥쪽 표피는 멜라닌을 함유한 거친 보호층입니다.
 
-as> q q is a harsh q containing melanine.
+as> "Outer epidermis" is a harsh "protective layer"  containing melanin.
 
-2. 인공기관은 잃어버린 신체를 대신하는 팔, 다리, 치아와 같은 인공 신체를 말힙니다.
+2. 인공기관은 잃어버린 신체를 대신하는 팔, 다리, 치아와 같은 인공 신체를 말합니다.
 
-as> Artificial q is an artificial body like arm, legs, tooth that q the body.
+as> Artificial "organ" is an artificial body like arm, legs, tooth that "replaces" the body.
 
 3. CPU는 세라믹이나 플라스틱 용기에 싸여 있습니다.
 
@@ -12,19 +12,19 @@ as> CPU is wrapped by a ceramic or plastic container.
 
 4. 그 약은 두 번의 임상시험에서 78 %의 치료율을 보였습니다.
 
-as> The q show 78% of q rate in q that performed twice.
+as> The medicine showed 78% of cure rate in clinical test that performed twice.
 
 5. 소프트웨어 스택은 어떤 결과를 도출하거나 공통 목표를 달성하기 위해 함께 동작하는 프로그램의 그룹입니다.
 
-as> Software stack is a group of programs that runs together to q a result or to accomplish a common q.
+as> Software stack is a group of programs that runs together to "draw" a result or to accomplish a common "goal".
 
 6. 그것들은 서로 평행인 채로 위치해 있습니다.
 
-as> These are located as q each other.
+as> These are located as "parallel" each other.
 
 7. 안장 기둥은 개인의 몸 비율에 맞출 수 있습니다.
 
-as> q can be fitted according to the body q of individual.
+as> "Saddle pillar" can be fitted according to the body "rate" of individual.
 
 8. 해당 서비스는 모바일 운영체제 기반 앱에 광고를 제공할 수 있습니다.
 
