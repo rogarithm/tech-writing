3f0c6ec3ea5b95762f8ae1b0db49cccc024c6f2b
sehun-kim <rogarithm@gmail.com>
2021-02-21 23:55:50 +0900

    m33 check usage and article, submit.

--- manual/m33.txt.pm
+++ manual/m33.txt.pm
@@ -1,37 +1,45 @@
 1. 바깥쪽 표피는 멜라닌을 함유한 거친 보호층입니다.
 
-as> Outer epidermis is a harsh protective layer containing melanin.
+as> The outer epidermis is a harsh protective layer that contains melanin.
+
+◊comment{"the outer epi..." because the term is explained specifically. Found that the definition for "epidermis" uses "the". Then what's the difference btw these?}
 
 2. 인공기관은 잃어버린 신체를 대신하는 팔, 다리, 치아와 같은 인공 신체를 말합니다.
 
-as> Artificial organ is an artificial body "such as" arm, leg, tooth that replaces lost natural organ.
+as> An artificial organ is an artificial body such as arms, legs, teeth that replaces a missing body part.
 
-◊comment{are the arm, leg, tooth plural?}
+◊comment{are the arm, leg, tooth plural? plural.}
+
+◊comment{"the artificial organ", same as the reason for "the outer epi..." of number 1. But wikipedia starts its explanation with "An".}
 
 3. CPU는 세라믹이나 플라스틱 용기에 싸여 있습니다.
 
-as> CPU is "wrapped up" with a ceramic or plastic container.
+as> The CPU is wrapped up with a ceramic or plastic container.
+
+◊comment{used a only once, because "ceramic" and "plastic" is close, making the scope of the article "a" clear.}
 
 4. 그 약은 두 번의 임상시험에서 78 %의 치료율을 보였습니다.
 
-as> The medicine showed 78% of cure rate in the clinical tests that were performed twice.
+as> The medicine showed 78% of cure rate from the two clinical tests.
 
 ◊link[https://www.donga.com/en/article/all/20010706/207910/1/Liver-Cancer-Medicine-Made-In-Korea-Approved]{source}
 
 5. 소프트웨어 스택은 어떤 결과를 도출하거나 공통 목표를 달성하기 위해 함께 동작하는 프로그램의 그룹입니다.
 
-as> Software stack is a group of programs that work together to deduce a result or to accomplish a common goal.
+as> A software stack is a group of programs that work together to deduce a result or to accomplish a common goal.
 
 6. 그것들은 서로 평행인 채로 위치해 있습니다.
 
-as> These are located "in" parallel each other.
+as> These are located in parallel "to" each other.
 
 7. 안장 기둥은 개인의 몸 비율에 맞출 수 있습니다.
 
-as> Seatpost can be set according to the body proportions of individuals.
+as> A seatpost can be adjusted "to" body proportions "in" individuals.
+
+◊comment{"seatpost" cannot be specified, so used "a"}
 
 ◊comment{Synonyms for saddle pillar: seatpost, seatpin, saddlepole, saddle pin}
 
 8. 해당 서비스는 모바일 운영체제 기반 앱에 광고를 제공할 수 있습니다.
 
-as> The "following" service may provide an advertisement to an app based on mobile operating system.
+as> The following service may provide an advertisement to mobile OS based apps.
