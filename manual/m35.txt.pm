b7c243de1a2225385af6e02af6a817b2cf7c07b4
sehun-kim <rogarithm@gmail.com>
2021-02-23 01:13:13 +0900

    m35 article check. submit.

--- manual/m35.txt.pm
+++ manual/m35.txt.pm
@@ -2,13 +2,19 @@
 
 as> The product is released with solid quality assurance and testing by the company.
 
+◊comment{quality assurance and testing can't be counted because these are generic, I guess}
+
 2. 회사는 품목별 소비자 분쟁해결 기준(공정거래위원회 고시)에 따라 다음과 같이 제품을 보증합니다.
 
 as> The company guarantees the product as follows according to the consumer dispute resolution regulation (Fair Trade Commission's Notice).
 
+◊link[https://www.grammarly.com/blog/period/]{rules about period}
+
 3. 제품에 대한 품질보증 기간은 구매 후 6개월로 규정되어 있습니다.
 
-as> The guarantee period of the product is "defined as" 6 months from the purchase.
+as> The guarantee period of the product is defined as 6 months from a purchase.
+
+◊comment{a purchase as it can't be specified yet.}
 
 4. 정상적인 사용 환경에서 고장이 발생했을 경우, 당사의 고객센터에서 무상 수리 또는 무상 교환해 드립니다.
 
@@ -16,17 +22,21 @@ as> For defects that occur under standard usage, you can get repair or exchange 
 
 5. 제조상의 결함이나 문의사항이 있으면, 고객센터로 문의하시기 바랍니다.
 
-as> If there's a manufacturing defect or questions to ask, contact customer center.
+as> If there's a manufacturing defect or questions to ask, contact the customer center.
 
 6. 보증 기간 만료 후 발생된 고장에 대해서는 소정의 수리비를 받고 수리해 드립니다.
 
 as> For a defect after expiration of guarantee period, repair will be performed at charge.
 
+◊comment{we cannot count expiration, so zero for expiration.}
+
 ◊link[http://www.sunhightech.co.kr/data/file/pro_1/979829585_uwrij6g5_KP-FD30CL_Specifications.pdf]{defect after expiration}
 
 7. 본 보증 규정 외의 사항에 대해서는 소비자보호법에 의거한 소비자 피해보상 규정을 따릅니다.
 
-as> Items not considered in the assurance regulation follows the consumer injury compensation rule in accordance with the consumer protection law.
+as> Items not considered in the assurance regulation follows the consumer injury compensation rule in accordance with the consumer protection laws.
+
+◊comment{Laws would suggest some laws, but not necessarily all of them. ◊link[https://english.stackexchange.com/questions/427168/should-it-be-law-or-laws-or-do-both-work/427179]}
 
 8. 본 제품 보증서는 재발행되지 않으므로 분실에 유의하시기 바랍니다.
 
