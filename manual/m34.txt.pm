a4b55e08b7af0147d3c9ced8b1ce90a89913ae01
sehun-kim <rogarithm@gmail.com>
2021-02-22 13:36:52 +0900

    m34 check usage and article.

--- manual/m34.txt.pm
+++ manual/m34.txt.pm
@@ -1,6 +1,6 @@
 1. 두 시간이 지나면 약효가 떨어집니다.
 
-as> The "potency of" medicine decreases after two hours.
+as> The "potency" is decreased after two hours.
 
 2. 스캔한 팩스를 한번에 57군데에 보낼 수 있습니다.
 
@@ -14,11 +14,11 @@ as> It's depth is 30 meters, and its area is over 2 million square meters.
 
 4. 토너는 토너 분말을 종이에 녹이기 위해 열을 사용합니다.
 
-as> A toner uses heat to melt toner powder "onto" paper.
+as> A toner uses heat to melt toner powder onto paper.
 
 5. 그 약은 복용 약입니다.
 
-as> The medicine is for take.
+as> The medicine is "for internal use".
 
 6. 회사가 1년간 무상 수리 서비스를 제공합니다.
 
@@ -26,7 +26,7 @@ as> The company provides free repair service for 1 year.
 
 7. 그것은 엔진이 힘들어 한다는 사실을 알려줍니다.
 
-as> It informs that the engine is tired.
+as> It means that the engine is tired.
 
 8. 정보 출처를 밝혀야 합니다.
 
