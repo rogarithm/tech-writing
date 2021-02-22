094bd88d17ab1a618c63faadf40b10cf867c1c86
sehun-kim <rogarithm@gmail.com>
2021-02-22 23:54:46 +0900

    m35 refine with usage search and check with "". add link for source.

--- manual/m35.txt.pm
+++ manual/m35.txt.pm
@@ -1,35 +1,39 @@
 1. 본 제품은 회사의 엄격한 품질관리 및 검사를 거쳐 출하되었습니다.
 
-as> The product is "shipped" through firm quality assurance and test of the company.
+as> The product is "released" with solid quality assurance and "testing" by the company.
 
 2. 회사는 품목별 소비자 분쟁해결 기준(공정거래위원회 고시)에 따라 다음과 같이 제품을 보증합니다.
 
-as> The company guarantees the product as follows according to the "consumer dispute settlement regulation" ("Fair Trade Commission Notice").
+as> The company guarantees the product as follows according to the consumer dispute "resolution" regulation (Fair Trade Commission"'s" Notice).
 
 3. 제품에 대한 품질보증 기간은 구매 후 6개월로 규정되어 있습니다.
 
-as> The quality assurance period for the product is regulated as 6 months after the purchase.
+as> The product quality assurance period expires in 6 months after the purchase.
 
 4. 정상적인 사용 환경에서 고장이 발생했을 경우, 당사의 고객센터에서 무상 수리 또는 무상 교환해 드립니다.
 
-as> When a defect occurs in normal "usage environment", you can get free-of-charge repair or free exchange in the customer center of "our company".
+as> For defects that occur "under standard usage", you can get free-of-charge repair or free exchange in the customer center of our company.
 
 5. 제조상의 결함이나 문의사항이 있으면, 고객센터로 문의하시기 바랍니다.
 
-as> For a defect on production or "enquiries", contact customer center.
+as> For a defect on production or enquiries, contact customer center.
 
 6. 보증 기간 만료 후 발생된 고장에 대해서는 소정의 수리비를 받고 수리해 드립니다.
 
-as> For a defect occured after expiration of assurance period, repair fee is required for repairment.
+as> For a defect after expiration of "guarantee period", "repair will be performed at charge".
+
+◊link[http://www.sunhightech.co.kr/data/file/pro_1/979829585_uwrij6g5_KP-FD30CL_Specifications.pdf]{defect after expiration}
 
 7. 본 보증 규정 외의 사항에 대해서는 소비자보호법에 의거한 소비자 피해보상 규정을 따릅니다.
 
-as> Items not considered in this assurance regulation follows the "consumerinjury compensation rule" depending on "consumer protection law".
+as> Items not considered in the assurance regulation follows the consumer injury compensation rule the consumer protection law.
 
 8. 본 제품 보증서는 재발행되지 않으므로 분실에 유의하시기 바랍니다.
 
-as> Careful for loss this "product warranty", as it can't be reprinted.
+as> Careful not to lose the product warranty, because it can't be reissued.
+
+◊link[https://images.homedepot-static.com/catalog/pdfImages/ab/ab0c7334-1c1a-4bc3-b60e-a3cbf3910403.pdf]{cannot be reissued}
 
 9. 수리 문의 시 본 제품 보증서를 꼭 제출하세요.
 
-as> You must submit this "product warranty" for repair contact.
+as> Make sure to submit the product warranty when asking to repair.
