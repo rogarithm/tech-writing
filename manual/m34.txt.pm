24c692b6468ab8c9d5bbdbddb707c6f79fb50f0d
sehun-kim <rogarithm@gmail.com>
2021-02-22 12:27:53 +0900

    m34 search words in dictionary

--- manual/m34.txt.pm
+++ manual/m34.txt.pm
@@ -1,6 +1,6 @@
 1. 두 시간이 지나면 약효가 떨어집니다.
 
-as> An effect of the medicine decreases after two hours.
+as> "A medicinal effect" of the medicine decreases after two hours.
 
 2. 스캔한 팩스를 한번에 57군데에 보낼 수 있습니다.
 
@@ -8,24 +8,24 @@ as> A scanned fax can be delivered to 57 places.
 
 3. 그것의 깊이는 30미터이고 넓이는 2백만 평방미터를 넘습니다.
 
-as> It's depth is 30 meters, and its q is over 2 million square meter|q|.
+as> It's depth is 30 meters, and its "area" is over 2 million square meter|q|.
 
 4. 토너는 토너 분말을 종이에 녹이기 위해 열을 사용합니다.
 
-as> A toner uses heat to melt toner q in paper.
+as> A toner uses heat to melt toner "powder" in paper.
 
 5. 그 약은 복용 약입니다.
 
-as> The medicine is for q.
+as> The medicine is for "dose".
 
 6. 회사가 1년간 무상 수리 서비스를 제공합니다.
 
-as> The company provides maintenance service without charge for 1 year.
+as> The company provides "repair" service without charge for 1 year.
 
 7. 그것은 엔진이 힘들어 한다는 사실을 알려줍니다.
 
-as> It informs that the engine q q.
+as> It informs that the engine "feels tired".
 
 8. 정보 출처를 밝혀야 합니다.
 
-as> Specify a source of the information.
+as> "Reveal" the source of information.
