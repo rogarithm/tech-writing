83f0f7989ec7bf17d2fbb1d0a2c78d0db5d2bb71
sehun-kim <rogarithm@gmail.com>
2021-02-23 13:09:33 +0900

    msg-1 search words from dic and check usage

--- msg/msg-1.txt.pm
+++ msg/msg-1.txt.pm
@@ -1,23 +1,23 @@
 1. 입력한 사용자명은 이미 사용 중입니다. 다시 입력하세요.
 
-as> Typed user name is assigned already. Type different value.
+as> Username you've entered is already taken by someone else. Enter another one.
 
 2. 값이 이미 지워지고 없습니다.
 
-as> The value is erased.
+as> The value was previously deleted.
 
 3. 작업이 진행 중입니다. 잠시만 기다려 주세요.
 
-as> Work is in progress. Hold a moment.
+as> Work is in progress. Please wait a while.
 
 4. 금지 특수문자를 사용해서는 안 됩니다.
 
-as> Do not use forbidden special character.
+as> Do not use forbidden special characters.
 
 5. 주소는 삭제 후 그룹에서만 지워지므로 주소를 지우려면 주소록에서 먼저 지워야 합니다.
 
-as> To delete an address, you need to delete it in q first because an address is erased only in group after deletion.
+as> To delete an address, delete it "from" address book first because an address is deleted only from group  after delete.
 
 6. 최근 변경 시간이 표시됩니다.
 
-as> It q the recent modification time.
+as> It shows the recent modification time.
