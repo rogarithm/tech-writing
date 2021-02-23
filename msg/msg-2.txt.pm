2529b1cfdaa3485baa814ef5ca2de9419e89205e
sehun-kim <rogarithm@gmail.com>
2021-02-23 22:50:59 +0900

    msg-2 search words from dic and check usage.

---
+++ msg/msg-2.txt.pm
@@ -0,0 +1,23 @@
+1. 현재 표시된 내용을 변경하시겠습니까?
+
+as> Do you want to modify currently indicated content?
+
+2. 현재의 설정값을 저장하시겠습니까?
+
+as> Would you like to save the current setting?
+
+3. 아무 값이나 선택하세요.
+
+as> Select any value.
+
+4. 파일이 바이러스에 감염되었습니다.
+
+as> The file is infected with a virus.
+
+5. 상신하지 않은 결재 항목이 있습니다.
+
+as> You have pending approvals.
+
+6. 잠시 후 다시 시도해 주세요.
+
+as> Please try again after a while.
