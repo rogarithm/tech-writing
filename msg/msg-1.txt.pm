e3d09804449a953ba33250da7731415725be5603
sehun-kim <rogarithm@gmail.com>
2021-02-23 13:31:02 +0900

    msg-1 article, style check. submit.

--- msg/msg-1.txt.pm
+++ msg/msg-1.txt.pm
@@ -1,6 +1,6 @@
 1. 입력한 사용자명은 이미 사용 중입니다. 다시 입력하세요.
 
-as> Username you've entered is already taken by someone else. Enter another one.
+as> The username you've entered is already taken by someone else. Please enter another one.
 
 2. 값이 이미 지워지고 없습니다.
 
@@ -8,7 +8,7 @@ as> The value was previously deleted.
 
 3. 작업이 진행 중입니다. 잠시만 기다려 주세요.
 
-as> Work is in progress. Please wait a while.
+as> The work is in progress. Please wait a while.
 
 4. 금지 특수문자를 사용해서는 안 됩니다.
 
@@ -16,7 +16,7 @@ as> Do not use forbidden special characters.
 
 5. 주소는 삭제 후 그룹에서만 지워지므로 주소를 지우려면 주소록에서 먼저 지워야 합니다.
 
-as> To delete an address, delete it "from" address book first because an address is deleted only from group  after delete.
+as> To delete an address, do it from the address book first, because a deleted address is removed only from the group.
 
 6. 최근 변경 시간이 표시됩니다.
 
