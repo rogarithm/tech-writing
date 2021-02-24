19b11f64302519c83635e787a1349f6ecae2e170
sehun-kim <rogarithm@gmail.com>
2021-02-24 13:26:02 +0900

    msg-3 search words, check usage.

--- msg/msg-3.txt.pm
+++ msg/msg-3.txt.pm
@@ -1,10 +1,12 @@
 1. 자료 수신이 지연되고 있습니다.
 
-as> Data transmission is now delayed.
+as> Data "receive" is now delayed.
+
+There's delay in receiving data.
 
 2. 일괄전송 완료!
 
-as> Transfer success!
+as> "Batch" transfer complete!
 
 3. 왼쪽 Tree에서 수정하고자 하는 Report Group을 선택해 주십시오.
 
@@ -12,12 +14,12 @@ as> Select the Report Group to modify from the left Tree.
 
 4. 누락된 설비를 재등록하십시오.
 
-as> Req qed appliance.
+as> Re-register "omitted" appliance.
 
 5. 1보다 큰 자연수를 입력하십시오.
 
-as> Type a natural number bigger than 1.
+as> Enter a natural number greater than 1.
 
 6. 계속하려면, ‘예’를 선택하십시오.
 
-as> To proceed, select 'yes'.
+as> To proceed, select "yes".
