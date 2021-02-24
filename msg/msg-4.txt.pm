99a08f8b293210cd1e7ea94e97c80a4d1d2dd11e
sehun-kim <rogarithm@gmail.com>
2021-02-25 08:53:07 +0900

    msg-4 initial writing

---
+++ msg/msg-4.txt.pm
@@ -0,0 +1,23 @@
+1. 삭제할 그룹이 이미 존재합니다. 그룹을 지우려면 등록된 정보가 없어야 합니다.
+
+as> The group to delete already exists. To delte the group, ◊uc{there should be} no registered information.
+
+2. 한 개 이상의 항목을 선택하십시오.
+
+as> Select ◊uc{more than} one item.
+
+3. 등록한 주소만 삭제할 수 있습니다. 삭제하려면 권한이 있어야 합니다. 
+
+as> Only registered address can be deleted. Authorization required for delete.
+
+4. 필수 파라미터 누락입니다. 
+
+as> q parameter is omitted.
+
+5. 파라미터가 유효하지 않습니다. 
+
+as> Parameter is unvalid .
+
+6. 알 수 없는 오류로 요청이 실패했습니다.
+
+as> Request failed due to unknown error.
