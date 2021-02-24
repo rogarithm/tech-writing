4b943541bd70231c27e979773d113d0bbb3f9628
sehun-kim <rogarithm@gmail.com>
2021-02-24 12:41:58 +0900

    msg-3 initial writing

---
+++ msg/msg-3.txt.pm
@@ -0,0 +1,23 @@
+1. 자료 수신이 지연되고 있습니다.
+
+as> Data transmission is now delayed.
+
+2. 일괄전송 완료!
+
+as> Transfer success!
+
+3. 왼쪽 Tree에서 수정하고자 하는 Report Group을 선택해 주십시오.
+
+as> Select the Report Group to modify from the left Tree.
+
+4. 누락된 설비를 재등록하십시오.
+
+as> Req qed appliance.
+
+5. 1보다 큰 자연수를 입력하십시오.
+
+as> Type a natural number bigger than 1.
+
+6. 계속하려면, ‘예’를 선택하십시오.
+
+as> To proceed, select 'yes'.
