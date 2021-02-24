766e1efa2b8b830a3744349de26fd3eb4792fca9
sehun-kim <rogarithm@gmail.com>
2021-02-24 14:37:36 +0900

    msg-3 check usage, submit.
     - check fdb for no.1

--- msg/msg-3.txt.pm
+++ msg/msg-3.txt.pm
@@ -1,12 +1,12 @@
 1. 자료 수신이 지연되고 있습니다.
 
-as> Data "receive" is now delayed.
-
-There's delay in receiving data.
+as> Data acquisition is being delayed.
 
 2. 일괄전송 완료!
 
-as> "Batch" transfer complete!
+as> Batch transfer has completed!
+
+◊link[https://docs.oracle.com/cd/E98457_01/opera_5_6_core_help/batch_transfer.htm]{oracle document}
 
 3. 왼쪽 Tree에서 수정하고자 하는 Report Group을 선택해 주십시오.
 
@@ -14,7 +14,9 @@ as> Select the Report Group to modify from the left Tree.
 
 4. 누락된 설비를 재등록하십시오.
 
-as> Re-register "omitted" appliance.
+as> Re-register lost appliance.
+
+◊link[https://www.ibm.com/support/knowledgecenter/SS9H2Y_7.7.0/com.ibm.dp.doc/considerations_firmware.html]{ibm}
 
 5. 1보다 큰 자연수를 입력하십시오.
 
@@ -22,4 +24,4 @@ as> Enter a natural number greater than 1.
 
 6. 계속하려면, ‘예’를 선택하십시오.
 
-as> To proceed, select "yes".
+as> To proceed, select "yes."
