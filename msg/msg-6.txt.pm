163ce106bd1aa5f3ac59ef66897ed5fe0e711f59
sehun-kim <rogarithm@gmail.com>
2021-02-25 22:52:30 +0900

    msg-6 initial writing

---
+++ msg/msg-6.txt.pm
@@ -0,0 +1,19 @@
+1. 가입 후에만 사용할 수 있는 서비스입니다.
+
+as> You can use this service after register.
+
+2. 해당 앱을 지속적으로 사용하면, 제품이 멈추거나 치명적인 오류가 발생합니다.
+
+as> Consistent use of the app make the product shut down or severe error occur.
+
+3. 제품 동작이 자꾸 멈추거나 느려지면, 실행 중인 애플리케이션을 종료하거나 전원을 껐다 켜 보세요.
+
+as> If the product stop or slow down often, exit running applications or turn the power off and on.
+
+4. 해당 전화번호에 대한 차단이 설정되어 있는지 확인하세요.
+
+as> Confirm the block to the phone number is set.
+
+5. 파일을 컴퓨터로 옮기거나 삭제해서 제품의 저장 공간을 확보하세요.
+
+as> Get storage space by deleting files or move them to computer.
