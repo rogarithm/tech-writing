051c45595a6be6db2e5690bbb0315ce4b754d025
sehun-kim <rogarithm@gmail.com>
2021-02-25 17:15:56 +0900

    msg-5 check usage, article. submit.

--- msg/msg-5.txt.pm
+++ msg/msg-5.txt.pm
@@ -4,20 +4,22 @@ as> Failed to authorize client.
 
 2. 접근 권한이 없습니다. 
 
-as> No permission for access.
+as> You have no permission to access.
 
 3. 해당 Lot 값은 이미 등록되어 있는 값이기 때문에 사용할 수 없습니다.
 
-as> The Lot value can't be used becuase it's already registered.
+as> The Lot value can't be used because it's already registered.
 
 4. 해당 리포트에 레시피가 하나 이상이므로, 리포트 복사를 진행할 수 없습니다.
 
-as> Cannot proceed to copy the report, because there's more than one recipe for the report.
+as> Unable to copy report, because there's more than one recipe for the report.
 
 5. Y축에 마진을 줄 수 없습니다.
 
-as> Cannot apply margin in Y axis.
+as> Unable to set margin in Y axis.
 
 6. 신호가 약하거나 수신 환경이 열악한 지역에서는 접속이 지연되거나 잘 되지 않을 수 있습니다.
 
-as> Access can be delayed or not well in a location where signal is weak or receive environment is weak.
+as> Access can be delayed or may not function well in a region where signal is weak or where coverage is poor.
+
+◊link[https://www.waveform.com/pages/causes-of-weak-signal]{poor coverage}
