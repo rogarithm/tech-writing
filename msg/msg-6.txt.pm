0dcb5fbd7ea310333f7403c6637c38043ce95958
sehun-kim <rogarithm@gmail.com>
2021-02-26 09:53:46 +0900

    msg-6 check article. submit.

--- msg/msg-6.txt.pm
+++ msg/msg-6.txt.pm
@@ -1,14 +1,14 @@
 1. 가입 후에만 사용할 수 있는 서비스입니다.
 
-as> You have to register to use this service.
+as> You have to register to use the service.
 
 2. 해당 앱을 지속적으로 사용하면, 제품이 멈추거나 치명적인 오류가 발생합니다.
 
-as> Using the app without a break makes the product stop or occurs severe error.
+as> Using the app without a break either makes the product stop or causes a severe error occurred.
 
 3. 제품 동작이 자꾸 멈추거나 느려지면, 실행 중인 애플리케이션을 종료하거나 전원을 껐다 켜 보세요.
 
-as> If the product stops or slows down often, exit running applications or turn the power off and on again.
+as> If the product stops or slows down often, exit ◊check{} running applications or turn the power off and on again.
 
 4. 해당 전화번호에 대한 차단이 설정되어 있는지 확인하세요.
 
@@ -16,4 +16,4 @@ as> Check the number is set to be blocked.
 
 5. 파일을 컴퓨터로 옮기거나 삭제해서 제품의 저장 공간을 확보하세요.
 
-as> Get the device's storage by deleting files or moving them to a computer.
+as> Get the device's storage by deleting ◊check{} files or moving them to a computer.
