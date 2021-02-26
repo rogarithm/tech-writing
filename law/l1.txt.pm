81259bc58af3c9b2d7f7730c6314f61d771ec115
sehun-kim <rogarithm@gmail.com>
2021-02-26 17:35:11 +0900

    Add law course. law 1 initial writing

---
+++ law/l1.txt.pm
@@ -0,0 +1,23 @@
+1. 계약이 무효가 된다.
+
+as> The contract has no effect.
+
+2. 기획서는 집행위에 상정되어야 한다.
+
+as> The report should be qed to q.
+
+3. 기획이 승인 후 구체화되어야 한다.
+
+as> The plan should be clarified after approval.
+
+4. 여러모로 볼 때, 그 선거는 사회의 표준에 적합하다.
+
+as> As seen in several aspect, the erection is proper to the society standard.
+
+5. 개인 정보는 다른 목적을 위해 사용할 수 있다.
+
+as> Personal information may be used for other purposes.
+
+6. 선적이 계약서에 명시된 날짜 안에 이루어질 수 없다.
+
+as> The shipping can't be made within date specified in the contract.
