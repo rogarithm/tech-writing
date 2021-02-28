ff138575a2d0d36a62d59af8c6017110e45b8c96
sehun-kim <rogarithm@gmail.com>
2021-02-28 15:30:10 +0900

    l1 check usage and search words from dic

--- law/l1.txt.pm
+++ law/l1.txt.pm
@@ -1,10 +1,10 @@
 1. 계약이 무효가 된다.
 
-as> The contract has no effect.
+as> The contract will have no effect.
 
 2. 기획서는 집행위에 상정되어야 한다.
 
-as> The report should be qed to q.
+as> The plan should be proposed to an executive committee.
 
 3. 기획이 승인 후 구체화되어야 한다.
 
@@ -12,7 +12,7 @@ as> The plan should be clarified after approval.
 
 4. 여러모로 볼 때, 그 선거는 사회의 표준에 적합하다.
 
-as> As seen in several aspect, the erection is proper to the society standard.
+as> In many ways, the erection is acceptable to society's standards.
 
 5. 개인 정보는 다른 목적을 위해 사용할 수 있다.
 
