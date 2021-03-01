a49eb507c835e3fb36bb284015200e61b5e0a231
sehun-kim <rogarithm@gmail.com>
2021-03-01 20:58:03 +0900

    l2 check usage and article. submit.

--- law/l2.txt.pm
+++ law/l2.txt.pm
@@ -1,6 +1,8 @@
 1. 법원이 유죄 평결을 번복했다.
 
-as> The court reversed the guilty verdict.
+as> Court returned a verdict of not guilty.
+
+◊comment{the court or just court?}
 
 2. 원본이 위조되었다.
 
@@ -8,16 +10,16 @@ as> The original is forged.
 
 3. 따로 쌍방간에 동의하지 않는 한, 사업 관련 정보는 비밀을 유지해야 한다.
 
-as> Business related information shall be kept as confidential if there's no mutual agreement.
+as> Business information shall be kept confidential unless the two sides agree.
 
 4. 회사는 무지로 인해 발생한 피해에 책임을 지지 않는다.
 
-as> The company isn't responsable for damage occurred from lack of knowledge.
+as> The company isn't responsible for any damages caused by ignorance.
 
 5. 그것은 미풍양속을 해치는 것이다.
 
-as> It harms traditional custom.
+as> It violates traditional custom.
 
 6. 그 조치는 위헌이다.
 
-as> The treatment is violation of the constitution.
+as> The action violates the constitution.
