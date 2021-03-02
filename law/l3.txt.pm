a7a3095e5d0bb4c9a15ab804b719c7d6e3639807
sehun-kim <rogarithm@gmail.com>
2021-03-02 14:39:13 +0900

    l3 check usage, article. submit.

--- law/l3.txt.pm
+++ law/l3.txt.pm
@@ -1,6 +1,6 @@
 1. 저작권 소유권은 원래 저자에게 있다.
 
-as> Copyright ownership belongs to the original author.
+as> The copyright ownership belongs to the original author.
 
 2. 그 회사들은 지주회사가 소유하고 있다.
 
@@ -8,7 +8,7 @@ as> The companies are owned by the holding company.
 
 3. 본 증서에 의하여 다음을 증명함.
 
-as> The following is verified by "the certificate".
+as> The following is verified by the certificate.
 
 4. 상기를 증명하기 위하여
 
@@ -18,8 +18,10 @@ as> In order to prove the above.
 
 as> The comapany assumes no legal responsibility or liability for any damage caused by user.
 
+◊comment{I guess user may be countable or not, depending on its detail}
+
 6. 회사가 면책을 못 면하는 경우, 회원은 초래된 손해에 대해 변상해야 한다.
 
-as> If the company is not be exempted from obligation, member shall compensate for all damages.
+as> When the company is not be exempted from obligation, member shall compensate for all damages.
 
 ◊comment{searched with "not * exempted from the obligation"}
