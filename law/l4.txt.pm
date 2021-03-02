ca48813381b73ce30481953eaff8530a062ed43e
sehun-kim <rogarithm@gmail.com>
2021-03-02 23:54:01 +0900

    l4 initial writing

---
+++ law/l4.txt.pm
@@ -0,0 +1,23 @@
+1. 회원이 프로그램의 버그를 악용한 경우, 회원은 초래된 손해에 대해 변상해야 한다.
+
+as> In case the member abuse a program bug, the member shall compensate the damage incurred.
+
+2. 귀사의 정보를 극비로 다룰 것을 확약한다.
+
+as> We confirm to treat your company information as confidential.
+
+3. 로열티는 도매가의 8 %이며 매년 지급된다.
+
+as> The loyalty is 8 % of the wholesale price and paid every year.
+
+4. 이 경우, 회사는 홈페이지에 자세한 사항을 공지한다.
+
+as> In this case, the company notify details on the homepage.
+
+5. 회원이 3자의 명의를 도용한 경우, 회사는 그것과 관련하여 아무 책임도 지지 않는다.
+
+as> When the member use someone's name illegaly, the company bears no liability with that.
+
+6. 그것은 불특정 다수에게 적용될 수 있다.
+
+as> It can be applied to anonymous individual.
