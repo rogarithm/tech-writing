6a79a3d3e81e9d1575b40d2021fd289b57b92fd1
sehun-kim <rogarithm@gmail.com>
2021-03-02 13:44:39 +0900

    l3 check usage. substitution for "certificate"?

--- law/l3.txt.pm
+++ law/l3.txt.pm
@@ -4,11 +4,11 @@ as> Copyright ownership belongs to the original author.
 
 2. 그 회사들은 지주회사가 소유하고 있다.
 
-as> The companies are owned by holding company.
+as> The companies are owned by the holding company.
 
 3. 본 증서에 의하여 다음을 증명함.
 
-as> The following is verified by the certificate.
+as> The following is verified by "the certificate".
 
 4. 상기를 증명하기 위하여
 
@@ -16,8 +16,10 @@ as> In order to prove the above.
 
 5. 회사는 사용자로 인해 초래된 어떤 상해에 대해서도 법적 책임을 지지 않는다.
 
-as> The comapany does not q legal responsibility for any damage from the user.
+as> The comapany assumes no legal responsibility or liability for any damage caused by user.
 
 6. 회사가 면책을 못 면하는 경우, 회원은 초래된 손해에 대해 변상해야 한다.
 
-as> If the company can't escape responsibility, member shall compensate the loss.
+as> If the company is not be exempted from obligation, member shall compensate for all damages.
+
+◊comment{searched with "not * exempted from the obligation"}
