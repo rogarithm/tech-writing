e7a0a7b2b5285e4558aef73c84413854dbf93fb1
sehun-kim <rogarithm@gmail.com>
2021-03-02 12:29:34 +0900

    l3 check usage

--- law/l3.txt.pm
+++ law/l3.txt.pm
@@ -1,6 +1,6 @@
 1. 저작권 소유권은 원래 저자에게 있다.
 
-as> Copyright ownership originally belongs to the author.
+as> Copyright ownership belongs to the original author.
 
 2. 그 회사들은 지주회사가 소유하고 있다.
 
@@ -8,11 +8,11 @@ as> The companies are owned by holding company.
 
 3. 본 증서에 의하여 다음을 증명함.
 
-as> Proves the following by the certificate.
+as> The following is verified by the certificate.
 
 4. 상기를 증명하기 위하여
 
-as> To prove q.
+as> In order to prove the above.
 
 5. 회사는 사용자로 인해 초래된 어떤 상해에 대해서도 법적 책임을 지지 않는다.
 
