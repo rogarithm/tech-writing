fee7b7538d9649235107d64a4ad7b01ed5d8499a
sehun-kim <rogarithm@gmail.com>
2021-03-02 10:56:49 +0900

    l3 initial writing

---
+++ law/l3.txt.pm
@@ -0,0 +1,23 @@
+1. 저작권 소유권은 원래 저자에게 있다.
+
+as> Copyright ownership originally belongs to the author.
+
+2. 그 회사들은 지주회사가 소유하고 있다.
+
+as> The companies are owned by holding company.
+
+3. 본 증서에 의하여 다음을 증명함.
+
+as> Proves the following by the certificate.
+
+4. 상기를 증명하기 위하여
+
+as> To prove q.
+
+5. 회사는 사용자로 인해 초래된 어떤 상해에 대해서도 법적 책임을 지지 않는다.
+
+as> The comapany does not q legal responsibility for any damage from the user.
+
+6. 회사가 면책을 못 면하는 경우, 회원은 초래된 손해에 대해 변상해야 한다.
+
+as> If the company can't escape responsibility, member shall compensate the loss.
