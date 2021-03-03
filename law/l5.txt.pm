8005054820714802e7266d7a30b7e5f1be29ee53
sehun-kim <rogarithm@gmail.com>
2021-03-03 12:04:25 +0900

    l5 check usage, article. submit.

---
+++ law/l5.txt.pm
@@ -0,0 +1,19 @@
+1. 회사가 면책될 수 없는 경우, 이와 관련하여 회사에 발생한 모든 손해을 배상해야 한다.
+
+as> If the company can't be exemtped, all damage caused by the company shall be compensated.
+
+2. 고객에 대한 어떤 정보도 알려줄 수 없다.
+
+as> Any information of client shall not be divulged.
+
+3. 이것은 3개월 미만의 피고용인들에게도 동일하게 적용된다.
+
+as> It's equivalently applied to employees with less than 3 months service.
+
+4. 권한을 즉시 고객에게 양도해야 한다.
+
+as> The authority shall immediately be transferred to the client.
+
+5. 진술한 일반사항을 침해하지 않으면서 우리는 그것들을 요구할 수 있다.
+
+as> We can request them without violating any general subjects that were stated.
