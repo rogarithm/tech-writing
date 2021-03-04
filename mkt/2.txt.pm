a1a8b752a675c0ba7af7ba89228bdb290c43601c
sehun-kim <rogarithm@gmail.com>
2021-03-04 16:27:38 +0900

    mkt-2 check usage

--- mkt/2.txt.pm
+++ mkt/2.txt.pm
@@ -1,14 +1,14 @@
 1. 현재 u-Healthcare 혹은 Mobile Healthcare는 전세계적으로 뜨는 핫키워드 용어이다.
 
-as> For now, u-Healthcare or Mobile Healthcare is a hot keyword terminology worldwide.
+as> For now, u-Healthcare or Mobile Healthcare is a hot keyword terminology rising worldwide.
 
 2. 의료분야에 m-to-m IT기술을 접목해서 병원에 오지 않고도 언제, 어디서나 진단과 치료 및 건강관리를 받을 수 있도록 하기 위한 개념이다.
 
-as> It's a concept made by connecting medical field to m-to-m IT technology to enable getting a diagnosis, treatment, and healthcare whenever and wherever without coming to hospital.
+as> It's a concept made by connecting medical field to m-to-m IT technology to enable getting a diagnosis, treatment, and healthcare anytime or anywhere without coming to hospital.
 
 3. 이 분야의 비즈니스는 현재 전세계적으로 급성장하고 있다.
 
-as> For now, the business in the field is grow massively worldwide.
+as> For now, the business in the field rapidly grows massively worldwide.
 
 4. 연평균 15 % 이상 성장하고 있고 북미와 유럽각국의 정부는 모두 고부가가치 서비스 산업으로 지정하고 있다.
 
