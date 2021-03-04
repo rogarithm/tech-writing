1a4860ad7c7f7e3c1621a5a47f5493410d61c272
sehun-kim <rogarithm@gmail.com>
2021-03-04 10:39:41 +0900

    mkt-1 article check

--- mkt/1.txt.pm
+++ mkt/1.txt.pm
@@ -1,6 +1,6 @@
 1. 2000년 2월 회사 설립 이후 세계 최고의 IP 솔루션들을 개발하기 위해 최선을 다해왔다.
 
-as> Since the foundation of company in february 2000, we've been doing our best to develop world leading IP solutions.
+as> Since the foundation of the company in february 2000, we've been doing our best to develop world leading IP solutions.
 
 2. 우리 회사는 NGN 분야의 전문 비즈니스 솔루션 제공업체이다.
 
@@ -8,7 +8,7 @@ as> Our company is a professional business solution provider in the field of NGN
 
 3. 우리 회사는 NGN 분야에서 주도적인 기업으로 성장했다.
 
-as> Our company has grown as leading company in the field of NGN.
+as> Our company has grown as a leading company in the field of NGN.
 
 4. 우리 회사는 그저 그런 평범한 회사가 아니다.
 
