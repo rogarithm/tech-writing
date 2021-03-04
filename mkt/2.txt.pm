6b4072b94e7bd25ad556492fcbd872777adae223
sehun-kim <rogarithm@gmail.com>
2021-03-04 23:57:37 +0900

    mkt-2 check usage, article, tense. submit.

--- mkt/2.txt.pm
+++ mkt/2.txt.pm
@@ -1,21 +1,21 @@
 1. 현재 u-Healthcare 혹은 Mobile Healthcare는 전세계적으로 뜨는 핫키워드 용어이다.
 
-as> For now, u-Healthcare or Mobile Healthcare is a hot keyword around the world.
+as> The terminology u-Healthcare or Mobile Healthcare is currently trending worldwide.
 
 2. 의료분야에 m-to-m IT기술을 접목해서 병원에 오지 않고도 언제, 어디서나 진단과 치료 및 건강관리를 받을 수 있도록 하기 위한 개념이다.
 
-as> It's a concept made by grafting m-to-m IT technology onto medical field to enable diagnosis, treatment, and healthcare anytime or anywhere without coming to hospital.
+as> It's a concept made by grafting m-to-m IT technology onto medical field to enable diagnosis, treatment, and healthcare anytime or anywhere without the need of visiting hospital.
 
 3. 이 분야의 비즈니스는 현재 전세계적으로 급성장하고 있다.
 
-as> For now, the business in the field rapidly grows around the world.
+as> For now, the business in the field is rapidly growing around the world.
 
 4. 연평균 15 % 이상 성장하고 있고 북미와 유럽각국의 정부는 모두 고부가가치 서비스 산업으로 지정하고 있다.
 
-as> It grows at an annual average of more than 15%, and selected as high value-added service industry by government of north america and various european countries.
+as> It has grown at an annual average of more than 15%, and has been selected as high value-added service industry by governments of north america and various european countries.
 
 ◊comment{"* high value added service"}
 
 5. 전통적 헬스케어 메디컬 관련 기업뿐만 아니라 모든 영역의 기업들이 이 분야를 신성장동력이라고 부르는데 주저함이 없다.
 
-as> "Companies of all field containing traditional healthcare medical" have no hesitation to call this field as a new growth engine.
+as> Companies in all fields, as well as in the field in traditional healthcare medical have no hesitation to call this field as a new growth engine.
