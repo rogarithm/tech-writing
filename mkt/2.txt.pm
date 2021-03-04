c800fb15f0834bb8ce9a4add0bd85af8b4e252dd
sehun-kim <rogarithm@gmail.com>
2021-03-05 00:01:04 +0900

    mkt-2 article check

--- mkt/2.txt.pm
+++ mkt/2.txt.pm
@@ -4,7 +4,7 @@ as> The terminology u-Healthcare or Mobile Healthcare is currently trending worl
 
 2. 의료분야에 m-to-m IT기술을 접목해서 병원에 오지 않고도 언제, 어디서나 진단과 치료 및 건강관리를 받을 수 있도록 하기 위한 개념이다.
 
-as> It's a concept made by grafting m-to-m IT technology onto medical field to enable diagnosis, treatment, and healthcare anytime or anywhere without the need of visiting hospital.
+as> It's a concept made by grafting m-to-m IT technology onto the medical field to enable diagnosis, treatment, and healthcare anytime or anywhere without the need of visiting a hospital.
 
 3. 이 분야의 비즈니스는 현재 전세계적으로 급성장하고 있다.
 
@@ -12,7 +12,7 @@ as> For now, the business in the field is rapidly growing around the world.
 
 4. 연평균 15 % 이상 성장하고 있고 북미와 유럽각국의 정부는 모두 고부가가치 서비스 산업으로 지정하고 있다.
 
-as> It has grown at an annual average of more than 15%, and has been selected as high value-added service industry by governments of north america and various european countries.
+as> It has grown at an annual average of more than 15%, and has been selected as a high value-added service industry by governments of north america and various european countries.
 
 ◊comment{"* high value added service"}
 
