d2240a55e374b5691c4e4278b058bbfd737cffbf
sehun-kim <rogarithm@gmail.com>
2021-03-05 12:22:33 +0900

    add in-flight-entertainment to article book. too many xs.

---
+++ others/article/in-flight-ent.html.pm
@@ -0,0 +1,13 @@
+#lang pollen
+
+definite?, countable?, singular?
+
+◊q[xox]{zero} Recent advancements in ◊q[xx]{zero} technology have significantly changed ◊q[ox]{the} way we are entertained during ◊q[xox]{zero} our travels.
+
+◊q[ooo]{the} first change is in ◊q[xx]{zero} equipment.
+
+Over ◊q[oox]{zero} last 15 years, ◊q[xox]{zero} airline companies have been spending ◊q[xox]{zero} millions of ◊q[xox]{zero} dollars per ◊q[xoo]{a} year equipping ◊q[xox]{zero} their planes with ◊q[oox]{the} built-in touch screens to play ◊q[xox]{zero} movies for ◊q[xox]{zero} their clientele.
+
+Additionally, ◊q[xx]{zero. general idea} internet access is having ◊q[xx]{zero. think it can be a, but impact isn't seem to be countable. If it could be, then is there a specific reason?} substantial impact on ◊q[ox]{the. context from above} in-flight entertainment.
+
+Within ◊q[ooo]{the} next decade, ◊q[xx]{zero} unlimited internet service will likely be available on ◊q[oox]{the} all international flights, which means ◊q[xox]{zero} passengers can check ◊q[xox]{zero} e-mails, watch ◊q[ooo]{zero. exception} Youtube videos, stream ◊q[ooo]{zero. exception} Netflix, and anything else they could do at ◊q[xx]{zero} home on ◊q[ox]{the} Internet.
