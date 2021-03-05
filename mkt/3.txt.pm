086a27216acc93b5d5d1cfa5f079995fdd3f9f4d
sehun-kim <rogarithm@gmail.com>
2021-03-05 16:08:18 +0900

    mkt-3 check usage.

--- mkt/3.txt.pm
+++ mkt/3.txt.pm
@@ -1,19 +1,19 @@
 1. 이스라엘과 더불어 초스피드 IT강국으로 손꼽히는 대한민국에서는 헬스케어 산업을 집중 육성하겠다고 정부지원 정책을 내놓았고 2013년 새 임기를 시작하는 대한민국 최초의 여성대통령인 박근혜씨는 이를 재천명했다.
 
-as> The Republic of Korea, considered as high speed IT nation along Israel, qed government support policy to incubate healthcare industry. Park Geun-hye, the first female president of the Republic of Korea vowed it again.
+as> The Republic of Korea, considered as high speed IT nation along Israel, "qed" government support policy to intensively incubate healthcare industry. Park Geun-hye, the first female president of the Republic of Korea, "stake it out" again.
 
 2. 기존 헬스케어 메디컬 산업이 의료인들을 중심으로 펼쳐졌다면 현재의 헬스케어산업은 mHeatlhcare로 스마트화, 개인화되고 있다.
 
-as> Existing healthcare medical industry was conducted medical staffs as its core, current healthcare industry become smart, personalize with mHealthcare.
+as> (q) Existing healthcare medical industry functioned with medical staffs as a center, q current healthcare industry "becomes smart" and personalized with mHealthcare.
 
 3. 스마트폰의 확산으로 인해 헬스케어와 메디컬 서비스의 시공간적 제약이 극복되고 있다.
 
-as> The space-time limitation of healthcare and medical service is overcome by spreading of smart phone.
+as> The space-time limitation of healthcare and medical service is overcome "by" diffusion of smartphones.
 
 4. 스마트 헬스케어라는 말로 대변되는 현재의 헬스케어 비즈니스가 기존 대형병원들에게는 그리 달갑지만은 않다.
 
-as> Existing big hospitals feels less welcome with current healthcare business that qed by the smart healthcare.
+as> Existing large hospitals "feels less welcome" with current healthcare business that represented by the smart healthcare.
 
 5. 오히려 대다수를 차지하는 중소병원 및 개인의원들이 이 기회를 잘 편승한다면 대형병원이 최첨단시설과 다양한 의료진을 무기로 지역 의료수요를  흡수하는 것을 꽤 완화시킬 수 있을 것이다.
 
-as> On the contrary, if middle or private hospitals jump on this chance, it can quite mitigate big hospital's absorbing local medical demand with high-end equipment and various medical staffs.
+as> On the contrary, if medium or private hospitals "leap at" this chance, it can quite mitigate big hospital's absorbing local medical demand with high-end equipment and various medical staffs.
