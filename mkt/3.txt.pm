9f68dc02548c836c95b811786b4537df83714ec2
sehun-kim <rogarithm@gmail.com>
2021-03-06 15:03:41 +0900

    mkt-3  check term consistency, refine style. submit.

--- mkt/3.txt.pm
+++ mkt/3.txt.pm
@@ -1,10 +1,10 @@
 1. 이스라엘과 더불어 초스피드 IT강국으로 손꼽히는 대한민국에서는 헬스케어 산업을 집중 육성하겠다고 정부지원 정책을 내놓았고 2013년 새 임기를 시작하는 대한민국 최초의 여성대통령인 박근혜씨는 이를 재천명했다.
 
-as> The Republic of Korea, considered as power high speed IT along with Israel, "brought in" government support policy to intensively incubate healthcare industry. Park Geun-hye, the first female president of South Korea whose "term starts" in 2013, "staked it out" again.
+as> South Korea, considered as an IT powerhouse along with Israel, brought in government support policy to intensively incubate healthcare industry. Park Geun-hye, the first female president of South Korea whose term starts in 2013, staked it out again.
 
 2. 기존 헬스케어 메디컬 산업이 의료인들을 중심으로 펼쳐졌다면 현재의 헬스케어산업은 mHeatlhcare로 스마트화, 개인화되고 있다.
 
-as> Whereas existing healthcare medical industry functioned with medical staffs as a center, current healthcare industry "is moving towards" smart and personalization with mHealthcare.
+as> Whereas existing healthcare medical industry were centered around medical staffs, current healthcare industry is moving towards smart and personalization with mHealthcare.
 
 3. 스마트폰의 확산으로 인해 헬스케어와 메디컬 서비스의 시공간적 제약이 극복되고 있다.
 
