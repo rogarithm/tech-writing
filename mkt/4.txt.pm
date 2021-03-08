250d4198e4af0b9c439228c73227aef40895ee0f
sehun-kim <rogarithm@gmail.com>
2021-03-08 13:14:41 +0900

    mkt-4 initial writing

---
+++ mkt/4.txt.pm
@@ -0,0 +1,19 @@
+1. 고령화 즉 평균수명의 연장으로 인해 건강한 삶의 니즈(에 대해서)는 충분하다 못해 폭발적으로 증가하고 있다.
+
+as> From aging, that is, extension of average life time, needs for healthy life grows explosively more than sufficient.
+
+2. 이 산업은 북미와 유럽, 호주, 일본, 한국뿐만 아니라 중국과 동남아시아 중남미 국가에서도 엄청난 잠재 시장을 형성할 것임에 틀림이 없다.
+
+as> The industry is certain to make a q potential market in not only North America, Europe, Australia, Japan, and Korea, but also in China and Southeast Asia, Middleq America countries.
+
+3. 한 독일 컨설팅회사는 2015년 전세계 스마트폰 사용자가 14억 명이 될 거라고 예측하고 있다.
+
+as> A consulting company in Germany anticipates the number of smartphone users will be 1.4 billion by 2015.
+
+4. 물론 전세계적 불황을 염두에 두고 예측한 숫자이다.
+
+as> The number is anticipated with consideration of worldwide q.
+
+5. 이중 약 30 %인  5억명이 mobile Healthcare관련 App을 이용할 것이라는 보고서를 제출했다.
+
+as> The submitted report qs that 0.5 billion, about 30 % of the number, will use the mobile Healthcare related App.
