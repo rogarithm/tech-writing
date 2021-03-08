960a37149cacbb754e524c37c2d51224ff0439cb
sehun-kim <rogarithm@gmail.com>
2021-03-08 15:26:17 +0900

    mkt-4 check usage. submit.

--- mkt/4.txt.pm
+++ mkt/4.txt.pm
@@ -1,10 +1,10 @@
 1. 고령화 즉 평균수명의 연장으로 인해 건강한 삶의 니즈(에 대해서)는 충분하다 못해 폭발적으로 증가하고 있다.
 
-as> From aging, average lifetime extension, needs for healthy life increases explosively beyond sufficient.
+as> From aging, that is, extension of average lifetime, demand for healthy life is increasing explosively beyond enough.
 
-2. 이 산업은 북미와 유럽, 호주, 일본, 한국뿐만 아니라 중국과 동남아시아 중남미 국가에서도 엄청난 잠재 시장을 형성할 것임에 틀림이 없다.
+2. 이 산업은 북미와 유럽, 호주, 일본, 한국뿐만 아니라 중국과 동남아시아, 중남미 국가에서도 엄청난 잠재 시장을 형성할 것임에 틀림이 없다.
 
-as> The industry is certain to form a huge potential market not only in North America, Europe, Australia, Japan, and Korea, but also in China and Southeast Asia, and Latin American countries.
+as> The industry is certain to form a huge potential market not only in North America, Europe, Australia, Japan, and Korea, but also in China, Southeast Asia, and Latin American countries.
 
 3. 한 독일 컨설팅회사는 2015년 전세계 스마트폰 사용자가 14억 명이 될 거라고 예측하고 있다.
 
@@ -16,4 +16,4 @@ as> The number is estimated with consideration of worldwide recession.
 
 5. 이중 약 30 %인 5억명이 mobile Healthcare관련 App을 이용할 것이라는 보고서를 제출했다.
 
-as> The report was submitted, stating that 500 million, about 30 % of the number, will use the mobile Healthcare App.
+as> The report was submitted, stating that 500 million, which is about 30 % of the number, will use the mobile Healthcare App.
