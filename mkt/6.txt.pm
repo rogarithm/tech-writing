d7dc0d0f9dd6ee7d24333bc4997bf7fce3755927
sehun-kim <rogarithm@gmail.com>
2021-03-09 17:38:47 +0900

    mkt-6 initial writing

---
+++ mkt/6.txt.pm
@@ -0,0 +1,19 @@
+1. 1년 6개월의 개발기간을 통해 마이크로칩의 크기는 현재 어린아이 신발 밑창에 들어갈 수 있을 정도로 소형화되었다.
+
+as> By 1 year and 6 months of develop period, the size of macro chip minimized to the size that could be entered into child shoe q.
+
+2. 2000년은 반도체 산업에 있어서 기념비적인 해였다.
+
+as> 2000 was a monumental year in semi-conductor industries.
+
+3. 만의 하나 있을까 말까 한 일이다.
+
+as> 
+
+4. IBM이 전자시장을 석권해 왔다.
+
+as> IBM has conquered the electronic market.
+
+5. 전체 시장의 80 %를 석권했다.
+
+as> It conquered 80 % of the whole market.
