6cd9a98ca60e108a1894a7674edc49f766911a7f
sehun-kim <rogarithm@gmail.com>
2021-03-09 12:55:59 +0900

    mkt-5 check usage and style. submit

--- mkt/5.txt.pm
+++ mkt/5.txt.pm
@@ -1,19 +1,23 @@
 1. Apple iPhone의 아성을 이미 흔들고 있는 대한민국의 스마트폰 가입자는 2012년 8월 기준으로 이미 3천만 명을 넘어섰다.
 
-as> The number of registered member of smartphone in Korea, where twists the reputation of Apple iPhone, was already over 30 million people based on 2012 August.
+as> In Korea, where shakes stronghold of Apple's iPhone, the number of registered smartphone users was already over 30 million people as of 2012 August.
 
 2. 모바일헬스케어는 단순히 병원내 업무 효율화에서만 적용되는 것이 아니라 예방적 치료, 웰니스, 의사-환자 커뮤니케이션 등 다양한 소비자 건강관리에 영향을 줄 것이라는 내용이다.
 
-as> Mobile healthcare is not only applied to make hospital job effective, but also affects to customer healthcare like preventive medical job, wellness, communication between doctor-patient, etc.
+as> Mobile healthcare is not only applied to streamline workflows in hospital, but also will influence on consumer healthcare like preventive care, wellness, doctor-patient communication, and so on.
+
+◊#{
+"streamline * in hospital"
+}
 
 3. 본 프로젝트는 지식기반 신산업을 지향하며 Product와 Service를 동시에 제공하는 Offering Business를 표방한다.
 
-as> The project aims information-based new industry, and represents the Offering Business that provides Product and Service at the same time.
+as> The project aims to set new industry based on information, and advocates for the Offering Business that provides Product and Service at the same time.
 
 4. 누워있거나 앉아있는 시간을 제외하고는, 우리의 일상생활은 걸음으로 시작해서 걸음으로 하루를 마친다.
 
-as> Except time when we sit or go to bed, people's daily life starts and end with walking.
+as> Except time spent sitting or lying down, people's daily lives start and end with walking.
 
 5. 영화 아바타를 찍기 위해 모션캡처 시스템을 이용했는데 원래는 의료용으로 먼저 활용 되었다.
 
-as> The motion capture system, formarly applied for medical purpose, was used to take a motion picture Avatar.
+as> The motion capture system, formerly used for medical purposes, was used to record a motion picture Avatar.
