403a930543a85c5c770915cc6808af6dd49be94c
sehun-kim <rogarithm@gmail.com>
2021-03-10 12:04:35 +0900

    mkt-6 additional grammar check

--- mkt/6.txt.pm
+++ mkt/6.txt.pm
@@ -1,10 +1,10 @@
 1. 1년 6개월의 개발기간을 통해 마이크로칩의 크기는 현재 어린아이 신발 밑창에 들어갈 수 있을 정도로 소형화되었다.
 
-as> Through 1 and a half years of development, currently the size of microchip get small enough to fit in a sole in children's shoes.
+as> Through 1 and a half years of development, currently the size of microchip is small enough to fit in a sole in children's shoes.
 
 2. 2000년은 반도체 산업에 있어서 기념비적인 해였다.
 
-as> 2000 was a monumental year for the semi-conductor industry.
+as> 2000 was a monumental year for the semiconductor industry.
 
 3. 만의 하나 있을까 말까 한 일이다.
 
