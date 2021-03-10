0e84f5bb70f038861f59035c1f016685c4f14007
sehun-kim <rogarithm@gmail.com>
2021-03-10 16:54:10 +0900

    mkt-7 initial writing + check usage

---
+++ mkt/7.txt.pm
@@ -0,0 +1,19 @@
+1. 올해의 수입액은 지난 4년간의 수입액과 비슷하다.
+
+as> The revenue for current year is nearly the same with the revenue of the last 4 years.
+
+ok 2. 그것이 태양계의 기원을 밝혀줄 수 있을지 모른다.
+
+as> It may clarify the origin of the solar system.
+
+ok 3. 시장이 이미 포화되었다.
+
+as> The market already reached saturation point.
+
+4. 우리는 성숙된 시장을 위해 준비해야 한다.
+
+as> We should be ready for mature markets.
+
+5. 상품에 대한 문의가 쇄도하고 있다.
+
+as> There's a deluge of inquiries about the product.
