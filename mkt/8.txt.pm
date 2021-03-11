4ed7b559824df5059fd6fd76853f6a54ed7e60e2
sehun-kim <rogarithm@gmail.com>
2021-03-11 16:05:23 +0900

    mkt-8 initial writing

---
+++ mkt/8.txt.pm
@@ -0,0 +1,19 @@
+1. 기획서가 집행위에 상정되었다.
+
+as> The plan paper is submitted to the constitutional committee
+
+2. 항상 협력해 주신 데 대해 감사하게 생각한다.
+
+as> Very grateful for cooperating
+
+3. 너무 오래 일하는 경우 생산성에서 역효과를 거둘 수 있다.
+
+as> It may cause a negative effect on productivity in case for too long work.
+
+4. 언젠가 컴퓨터가 우리의 생활을 접수할 날이 있을 것이다.
+
+as> 
+
+5. 그 회사가 지난 3개월에 걸쳐 최고 기록을 보여주고 있다.
+
+as> 
