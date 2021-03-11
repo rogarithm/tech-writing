35708891b904f590b3f006a6eae144719c87d930
sehun-kim <rogarithm@gmail.com>
2021-03-11 21:50:10 +0900

    mkt-8 check usage. last sentence is difficult to find examples

--- mkt/8.txt.pm
+++ mkt/8.txt.pm
@@ -1,6 +1,6 @@
 1. 기획서가 집행위에 상정되었다.
 
-as> The plan is proposed to the executive committee.
+as> The plan was proposed to the executive committee.
 
 2. 항상 협력해 주신 데 대해 감사하게 생각한다.
 
@@ -8,12 +8,12 @@ as> Thnaks for the cooperation and assistance all the time.
 
 3. 너무 오래 일하는 경우 생산성에서 역효과를 거둘 수 있다.
 
-as> Working too long may cause a negative effect on productivity in case .
+as> Working too long may cause the opposite effect of productivity.
 
 4. 언젠가 컴퓨터가 우리의 생활을 접수할 날이 있을 것이다.
 
-as> Someday computers will dominate our lives.
+as> Someday computer will become an inseparable part of our lives.
 
 5. 그 회사가 지난 3개월에 걸쳐 최고 기록을 보여주고 있다.
 
-as> The company show the top record on the last 3 months.
+as> The company "show the top record for" the last 3 months.
