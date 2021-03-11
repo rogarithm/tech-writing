4af977f730dceeba95ba18eefb0a23d3eef28feb
sehun-kim <rogarithm@gmail.com>
2021-03-11 21:21:45 +0900

    mkt-8 check usage

--- mkt/8.txt.pm
+++ mkt/8.txt.pm
@@ -1,19 +1,19 @@
 1. 기획서가 집행위에 상정되었다.
 
-as> The plan paper is submitted to the constitutional committee
+as> The plan is proposed to the executive committee.
 
 2. 항상 협력해 주신 데 대해 감사하게 생각한다.
 
-as> Very grateful for cooperating
+as> Thnaks for the cooperation and assistance all the time.
 
 3. 너무 오래 일하는 경우 생산성에서 역효과를 거둘 수 있다.
 
-as> It may cause a negative effect on productivity in case for too long work.
+as> Working too long may cause a negative effect on productivity in case .
 
 4. 언젠가 컴퓨터가 우리의 생활을 접수할 날이 있을 것이다.
 
-as> 
+as> Someday computers will dominate our lives.
 
 5. 그 회사가 지난 3개월에 걸쳐 최고 기록을 보여주고 있다.
 
-as> 
+as> The company show the top record on the last 3 months.
