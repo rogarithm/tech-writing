ee4fb7a91ee7265b399274e145e9d4058765a75b
sehun-kim <rogarithm@gmail.com>
2021-03-16 12:48:37 +0900

    mkt-9 initial writing

---
+++ mkt/9.txt.pm
@@ -0,0 +1,19 @@
+1. 새로운 연구는 기존 연구보다 조금 더 나을 뿐이다.
+
+as> New research is just slightly better than the conventional one.
+
+2. 사업이 비약적인 발전을 할 것으로 보인다.
+
+as> The business seems to develop rapidly.
+
+3. 빨라도 그때까지 출시하지 못할 것 같다.
+계획보다 페이스가 빠르더라도
+as> The product cannot be released even if it's fast.
+
+4. 회사가 이 분야에서 엄청난 잠재력을 가지고 있다.
+
+as> The company has huge potential in the field.
+
+5. 그들은 작년 막대한 손실을 이월 받았다.
+
+as> They've huge loss carried from last year.
