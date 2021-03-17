dfd4c4c2bd03acbefc2bf00e7495d8cf9f7d7f7d
sehun-kim <rogarithm@gmail.com>
2021-03-17 14:38:38 +0900

    mkt-10 initial writing. check usage.

---
+++ mkt/10.txt.pm
@@ -0,0 +1,19 @@
+1. 고품질의 제품을 낮은 가격에 계속 제공할 것이다.
+
+as> We'll keep provide high-quality products at a low price.
+
+2. 사업을 활성화시킬 방안을 찾아야 한다.
+
+as> We must find ways to make the business active.
+
+3. 이 글로벌 시장은 발전할 가능성이 크다.
+
+as> The global market has huge potential of development.
+
+4. 우리는 이 회사를 본 궤도에 올려놓고 싶어한다.
+
+as> We want to put the company on the right track.
+
+5. 기존 가격의 10분의 1에 제품을 공급할 수 있다
+
+as> The product can be provided at one-tenth of the current price.
