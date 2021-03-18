fa8701689def87178d3ffe879736734e2187c922
sehun-kim <rogarithm@gmail.com>
2021-03-18 11:27:00 +0900

    mkt-11 initial writing, check usage and article.

---
+++ mkt/11.txt.pm
@@ -0,0 +1,23 @@
+1. 최근까지도 밀그램의 초기 실험을 제외하고는 실증 실험이 거의 이루어지지 않았다.
+
+as> Until recently, there are only a few proof experiments done except milgram's original experiment.
+
+2. 수많은 회사들이 새로운 약품에 대한 실험을 지원하고 있다.
+
+as> Numerous companies are supporting experiments for new medicine.
+
+ok 3. 몇 달간의 각고의 노력 끝에 본 회계 프로그램을 개발했다.
+
+as> With months of effort, the accounting program was developed.
+
+4. 뇌와 대형 컴퓨터 사이의 유사성을 끌어냈다.
+
+as> Similarity between the brain and a mainframe computer is extracted.
+
+5. 유방암은 조기에만 발견되면 싹을 죽일 수 있다.
+
+as> Breast cancer can be wiped out as long as it's detected early.
+
+ok 6. 그것은 좋은 비즈니스 관례의 모범답안이다.
+
+as> That is a model of good business practice.
