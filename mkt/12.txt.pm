4220e0093bc8a6d17887bb987313f4ffae76978c
sehun-kim <rogarithm@gmail.com>
2021-03-18 22:51:03 +0900

    mkt-12 check usage.

--- mkt/12.txt.pm
+++ mkt/12.txt.pm
@@ -1,23 +1,23 @@
 1. 그 빌딩의 정면을 따라 아홉 개의 아치가 있다.
 
-as> The building has 9 arches along with the front of it.
+as> There're 9 arches along the front of the building.
 
 2. 그 빌딩은 엘리베이터가 없는 3층 건물이다.
 
-as> It is 3 floor building with no elevator.
+as> The building has 3 floors without elevator.
 
 3. 신체의 치료 체계와 신념 체계는 밀접하게 연관되어 있다.
 
-as> Medicine system for body and belief system are highly connected.
+as> Immune system and belief system are tightly linked together.
 
 4. 디자인한 옷이 세계 패션계에 돌풍을 일으켰다.
 
-as> The designed cloth made q in world fasion.
+as> The designed cloth made a sensation in the global fashion industry.
 
 5. 그것은 공학기술과 건축술의 경이적인 업적을 보여준다.
 
-as> It represents tremendous work of engineering technique and work of architechture.
+as> It is an impressive work of engineering and architecture.
 
 6. 이곳은 서울의 명동에 해당되는 지역이다.
 
-as> The region is of Myung-dong of Seoul
+as> This region is Myeongdong of Seoul.
