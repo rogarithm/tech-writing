51d6585ba8f3cc1da5e0f1ab5e0ddffff193dc81
sehun-kim <rogarithm@gmail.com>
2021-03-18 17:18:11 +0900

    mkt-12 initial writing

---
+++ mkt/12.txt.pm
@@ -0,0 +1,23 @@
+1. 그 빌딩의 정면을 따라 아홉 개의 아치가 있다.
+
+as> The building has 9 arches along with the front of it.
+
+2. 그 빌딩은 엘리베이터가 없는 3층 건물이다.
+
+as> It is 3 floor building with no elevator.
+
+3. 신체의 치료 체계와 신념 체계는 밀접하게 연관되어 있다.
+
+as> Medicine system for body and belief system are highly connected.
+
+4. 디자인한 옷이 세계 패션계에 돌풍을 일으켰다.
+
+as> The designed cloth made q in world fasion.
+
+5. 그것은 공학기술과 건축술의 경이적인 업적을 보여준다.
+
+as> It represents tremendous work of engineering technique and work of architechture.
+
+6. 이곳은 서울의 명동에 해당되는 지역이다.
+
+as> The region is of Myung-dong of Seoul
