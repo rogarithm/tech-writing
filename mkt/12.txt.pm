2a186a9f1098ec49efcdb2a97c6c398a59a44879
sehun-kim <rogarithm@gmail.com>
2021-03-19 10:25:35 +0900

    mkt-12 check usage, article. submit.

--- mkt/12.txt.pm
+++ mkt/12.txt.pm
@@ -8,11 +8,11 @@ as> The building has 3 floors without elevator.
 
 3. 신체의 치료 체계와 신념 체계는 밀접하게 연관되어 있다.
 
-as> Immune system and belief system are tightly linked together.
+as> The immune system and belief system are tightly linked together.
 
 4. 디자인한 옷이 세계 패션계에 돌풍을 일으켰다.
 
-as> The designed cloth made a sensation in the global fashion industry.
+as> The designed clothes made a sensation in the global fashion industry.
 
 5. 그것은 공학기술과 건축술의 경이적인 업적을 보여준다.
 
@@ -20,4 +20,4 @@ as> It is an impressive work of engineering and architecture.
 
 6. 이곳은 서울의 명동에 해당되는 지역이다.
 
-as> This region is Myeongdong of Seoul.
+as> The region belongs to Myeongdong, Seoul.
