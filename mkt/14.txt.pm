73ffd672d591f53ea5ed1b035da8cabe9a0feaeb
sehun-kim <rogarithm@gmail.com>
2021-03-22 14:19:14 +0900

    mkt-14 initial writing

---
+++ mkt/14.txt.pm
@@ -0,0 +1,23 @@
+1. 2시간마다 정각에 페리가 있다.
+
+as> The ferries are at every 2 hours.
+
+2. 기술이 성숙했다.
+
+as> The technology got matured.
+
+3. 올해의 오렌지 수확이 작년보다 약간 더 적을지 모른다.
+
+as> The yield of orange of this year might be lower than the previous year.
+
+4. 그런 추세가 계속 진행되어서 CDMA 관련 사업을 하나로 묶을지도 모른다.
+
+as> If the tendency continues, CDMA industries might be merged.
+
+5. 사업이 잘 진행되고 있다.
+
+as> The business is going well.
+
+6. 투자자들이 사업 전망에 대해 매우 낙관한다.
+
+as> The investors are very delightful about the business.
