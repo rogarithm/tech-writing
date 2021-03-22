49f9afa87601ab26a570bd25599940df9e9ac4f4
sehun-kim <rogarithm@gmail.com>
2021-03-22 11:55:08 +0900

    mkt-13 check usage, article, submit.

---
+++ mkt/13.txt.pm
@@ -0,0 +1,23 @@
+1. 통화 약세가 수출 증가를 도왔다.
+
+as> Weak currency led to stronger exports.
+
+2. 우리는 전체 세트들을 가지고 있다.
+
+as> We provide the whole suite.
+
+3. 혹한기가 지나면, 가격이 내릴 것이다.
+
+as> The price will decrease after extreme cold weather.
+
+4. 대형 슈퍼들은 자금 회전률이 높다.
+
+as> Supermarkets have high stock turnover ratio.
+
+5. 바로 몇 년 전에 전문가들이 미국 산업의 침체를 선언했다.
+
+as> Just a few years ago, experts declared the decline of American industries.
+
+6. 재고를 없애기 위해 원가로 TV 수상기를 팔았다.
+
+as> The TV receiver was sold at its cost price to clear stock.
