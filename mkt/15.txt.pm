abcd231f8bce496fd230f17fe570661f30b68c5c
sehun-kim <rogarithm@gmail.com>
2021-03-22 23:39:36 +0900

    mkt-15 initial writing

---
+++ mkt/15.txt.pm
@@ -0,0 +1,23 @@
+1. 이 재산은 당신 제안의 3배 가치를 지닌다.
+
+as> This property has three times of value that you've offered.
+
+2. 경제자유화에 따라 약간의 여유를 갖게 되었다.
+
+as> Economic free gave us little composure.
+
+3. 모든 주요 경제 주체들이 기후 변화에 대한 고비를 넘기기 시작할 것이다.
+
+as> Every main economic representatives overcome the difficulties of climate change.
+
+4. 회사를 상장할 것이다.
+
+as> The company will be taken public.
+
+5. 이제 상승세를 타고 있다.
+
+as> It's now on the fly.
+
+6. 점점 더 많은 소규모 회사들이 사무실 장비 임대를 택하고 있다.
+
+as> More and more small sized companies are choosing equipment rent for their offices.
