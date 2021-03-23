565527252e9421cee8f87a6bc024a16d03bd2253
sehun-kim <rogarithm@gmail.com>
2021-03-23 12:18:54 +0900

    mkt-15 check usage, article. submit.

--- mkt/15.txt.pm
+++ mkt/15.txt.pm
@@ -1,14 +1,14 @@
 1. 이 재산은 당신 제안의 3배 가치를 지닌다.
 
-as> This property has three times of value that you've offered.
+as> This property is worth three times as much as your offer.
 
 2. 경제자유화에 따라 약간의 여유를 갖게 되었다.
 
-as> Economic free gave us little composure.
+as> Economic freedom gave us a little piece of mind.
 
 3. 모든 주요 경제 주체들이 기후 변화에 대한 고비를 넘기기 시작할 것이다.
 
-as> Every main economic representatives overcome the difficulties of climate change.
+as> All key economic actors will start to overcome the climate crisis.
 
 4. 회사를 상장할 것이다.
 
@@ -16,8 +16,8 @@ as> The company will be taken public.
 
 5. 이제 상승세를 타고 있다.
 
-as> It's now on the fly.
+as> Now it shows an upward tendency.
 
 6. 점점 더 많은 소규모 회사들이 사무실 장비 임대를 택하고 있다.
 
-as> More and more small sized companies are choosing equipment rent for their offices.
+as> More and more small companies are leasing office equipment rather than purchasing it.
