ca4fc5eccd73db03bfb55a3ea2ecbeeee5d6d757
sehun-kim <rogarithm@gmail.com>
2021-03-23 19:21:37 +0900

    mkt-17 initial writing

---
+++ mkt/17.txt.pm
@@ -0,0 +1,23 @@
+1. 우리는 요즘 타성에 젖어 있었다.
+
+as> We've been in our inertia.
+
+2. 우리는 진행에 차질을 경험했다.
+
+as> We've encountered an obstacle in the progress.
+
+3. 우리는 고객과의 거래를 성사시켜야 한다.
+
+as> The deal must be made.
+
+4. 우리는 정면 승부를 해야 한다.
+
+as> We must direct fight.
+
+5. 위성 안테나의 도입은 세계 최고의 이 인구밀집 지대를 세계 최대 TV 시청자 집단으로 급속히 바꾸고 있다.
+
+as> Application of satellite anttena is changing the population condensed region of the world into TV watcher group best of the world.
+
+6. 휴대폰에 대한 무선 충전 기술의 상용화는 아직도 갈 길이 멀다.
+
+as> Wireless cellphone charging technology has long way to go.
