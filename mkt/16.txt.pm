fdd3ead214c4410a06d2d2eb8bc233395d492f46
sehun-kim <rogarithm@gmail.com>
2021-03-23 14:34:33 +0900

    mkt-16 initial writing

---
+++ mkt/16.txt.pm
@@ -0,0 +1,23 @@
+1. 매출이 비약적으로 성장했다.
+
+as> The revenue have grown extremely.
+
+2. 그들이 세게 나왔지만 결국 우리가 우위를 점했다.
+
+as> Though their toughness, we have took the priority.
+
+3. 해당 내용이 원본과 근본적으로 다르다.
+
+as> The corresponding content differs from the genuine from its root.
+
+4. 특별 제품들은 고객들만 이용할 수 있고 다른 보상 프로그램과 연계해서 사용할 수 없다.
+
+as> Special products is only for customers and cannot used in accordance with other compensation program.
+
+5. 우리는 드디어 세계 시장의 관심을 얻어냈다.
+
+as> We've finally taken world market's attention.
+
+6. 그 점에서 우리는 똑같다.
+
+as> We are the same in the point.
