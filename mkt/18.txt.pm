6ebb5c399e91c55aa7a9bb0f7d109cc9a93c9c18
sehun-kim <rogarithm@gmail.com>
2021-03-24 16:11:01 +0900

    mkt-18 initial writing

---
+++ mkt/18.txt.pm
@@ -0,0 +1,23 @@
+1. 이 기술의 성공은 어떻게 약점을 보완하느냐에 달려 있다.
+
+as> The success of the technology depends on the way of q of its weak point.
+
+2. 얼어붙었던 시장이 풀리고 있다.
+
+as> Market is getting energy.
+
+3. 무접점 충전시스템에 대한 열띤 토론이 지속되고 있다.
+
+as> Hot debate is ongoing about wireless charging system.
+
+4. 창조적이면서 독창적인 계획이 곧 만들어질 것이다.
+
+as> Soon a creative and impressive plan will be made.
+
+5. 미국에서는 디지털 서적의 매출이 인쇄 서적의 매출을 앞지르고 있다.
+
+as> In America, Sales in digital books is forwarding in printed books.
+
+6. 아무 것도 정해지지 않았다.
+
+as> Nothing has determined yet.
