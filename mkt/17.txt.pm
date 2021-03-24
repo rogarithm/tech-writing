c38fd3a7aa42b6b96e91f5a48ceaee7d01058daf
sehun-kim <rogarithm@gmail.com>
2021-03-24 12:59:15 +0900

    mkt-17 check usage and article. submit.

--- mkt/17.txt.pm
+++ mkt/17.txt.pm
@@ -1,10 +1,10 @@
 1. 우리는 요즘 타성에 젖어 있었다.
 
-as> We've been in our inertia.
+as> Recently we've been held back by inertia.
 
 2. 우리는 진행에 차질을 경험했다.
 
-as> We've encountered an obstacle in the progress.
+as> We've encountered an obstacle during the project.
 
 3. 우리는 고객과의 거래를 성사시켜야 한다.
 
@@ -12,12 +12,12 @@ as> The deal must be made.
 
 4. 우리는 정면 승부를 해야 한다.
 
-as> We must direct fight.
+as> We have to face the problem.
 
 5. 위성 안테나의 도입은 세계 최고의 이 인구밀집 지대를 세계 최대 TV 시청자 집단으로 급속히 바꾸고 있다.
 
-as> Application of satellite anttena is changing the population condensed region of the world into TV watcher group best of the world.
+as> Introducing the satellite antenna is rapidly changing the world's most densely populated region into the largest group of TV viewers around the world.
 
 6. 휴대폰에 대한 무선 충전 기술의 상용화는 아직도 갈 길이 멀다.
 
-as> Wireless cellphone charging technology has long way to go.
+as> Popularization of wireless charging for cell phones has long way to go.
