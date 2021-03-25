fa0bef7b86af94ece0c998b6772036da3c66e577
sehun-kim <rogarithm@gmail.com>
2021-03-25 11:13:12 +0900

    mkt-18 check usage, article. submit:
     - name of technology is definitive
     - exceeded -> has exceeded

--- mkt/18.txt.pm
+++ mkt/18.txt.pm
@@ -1,6 +1,6 @@
 1. 이 기술의 성공은 어떻게 약점을 보완하느냐에 달려 있다.
 
-as> The success of the technology depends on the way of supplement of its weak point.
+as> The success of the technology depends on how to supplement its weak point.
 
 2. 얼어붙었던 시장이 풀리고 있다.
 
@@ -8,7 +8,7 @@ as> Market is recovering from recession.
 
 3. 무접점 충전시스템에 대한 열띤 토론이 지속되고 있다.
 
-as> Heated debate on non-contact charging system is ongoing.
+as> Heated debate is ongoing on the non-contact charging system.
 
 4. 창조적이면서 독창적인 계획이 곧 만들어질 것이다.
 
@@ -16,7 +16,7 @@ as> A creative and inventive plan soon will be made.
 
 5. 미국에서는 디지털 서적의 매출이 인쇄 서적의 매출을 앞지르고 있다.
 
-as> In America, sales of e-books exceeded the sales of paperback.
+as> In America, sales of e-books has exceeded the sales of paperback.
 
 6. 아무 것도 정해지지 않았다.
 
