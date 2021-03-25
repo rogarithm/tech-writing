d585771ebd9cc17e8e7a6d93fee9a1570e96d8cb
sehun-kim <rogarithm@gmail.com>
2021-03-25 15:22:24 +0900

    mkt-19 additional grammar check

--- mkt/19.txt.pm
+++ mkt/19.txt.pm
@@ -8,11 +8,11 @@ as> Technology advances frequently.
 
 3. 지금이 절호의 기회이다.
 
-as> Now is a perfect chance.
+as> Now is the perfect chance.
 
 4. 그들이 사업을 무산시키려고 집요하게 매달려왔다.
 
-as> They have stubbornly tried to destroy the business.
+as> They have stubbornly tried to abort the business.
 
 5. 그들의 계획이 교착상태에 빠졌다.
 
