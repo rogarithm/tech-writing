c3d927e767c6374aeeeea57a652ffc3d63400c9a
sehun-kim <rogarithm@gmail.com>
2021-03-25 15:15:51 +0900

    mkt-19 initial writing+check usage

---
+++ mkt/19.txt.pm
@@ -0,0 +1,19 @@
+1. 일반인들에게 책을 열람시키고 있다.
+
+as> Public is allowed to view books.
+
+2. 기술 발전이 아주 빈번하게 일어나고 있다.
+
+as> Technology advances frequently.
+
+3. 지금이 절호의 기회이다.
+
+as> Now is a perfect chance.
+
+4. 그들이 사업을 무산시키려고 집요하게 매달려왔다.
+
+as> They have stubbornly tried to destroy the business.
+
+5. 그들의 계획이 교착상태에 빠졌다.
+
+as> Their plan has come to a standstill.
